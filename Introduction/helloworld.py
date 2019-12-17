08131e636a29579945283686ff70b153fa36d538
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-17 18:52:40 +0530

    Learn more about print function

--- Introduction/helloworld.py
+++ Introduction/helloworld.py
@@ -1,5 +1,9 @@
 print('Hello, World!')
+# To understand the print function, what we are doing here is passing an argument to the print function.
+# Print function in Python can accept several argument irrespective of its data type.
 print(1 +7)
 print(100 * 2.5)
 print()
-print("The End!")
+# We can print multiple strings on Python. Refer to below example.
+# Here each argument is differentiated with comma.
+print("The End!", "Or is it?", "Keep watching to learn Python", 3)
