a72b38859883ebb897a26f7cb34017bb090a9bde
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-17 19:10:17 +0530

    Learn about strings in Python

---
+++ Introduction/strings.py
@@ -0,0 +1,13 @@
+# In this lesson we are going to learn more about the strings in Python.
+# Strings in Python can be printed either using single quotes or double quotes.
+# we can also concatenate strings in Python using the +
+print("Hello World")
+print('Python is fun')
+print('Learning Python Masterclass is the best python course on Udemy')
+print("Python's is fun")
+print('We can add "" inside single quotes')
+print('This is string' + ' concatenation')
+# We can also variables to the strings. Refer to the below example.
+greeting = "Hello"
+name = "Deepanshu!"
+print(greeting + ' ' + name)
