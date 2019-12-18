d6523257da708636bf23eea4fae0491c8c2b6d32
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-18 19:09:17 +0530

    Negative indexing in string

--- Data Types/stringDataType.py
+++ Data Types/stringDataType.py
@@ -3,3 +3,11 @@
 parrot = "Norwegian Blue"
 print(parrot)
 print(parrot[3])
+
+# In the above example, the number in square paranthesis is call index number.
+
+# Below is an example of negative index of a string
+
+print(parrot[-4])
+print(parrot[-5])
+print(parrot[-6])
