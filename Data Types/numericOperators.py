8a46f8f40691973e3109d777bd1737ee31642448
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-18 18:29:58 +0530

    Learn about numeric operators

---
+++ Data Types/numericOperators.py
@@ -0,0 +1,11 @@
+# In this lesson we are going to learn about the numeric operators in the Python.
+
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
