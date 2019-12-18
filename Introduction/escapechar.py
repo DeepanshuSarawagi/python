f5933d720454b6cdc9b605eca9f7eda071232ef5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-18 17:50:25 +0530

    Learn about the escape char in python

---
+++ Introduction/escapechar.py
@@ -0,0 +1,15 @@
+# In this lesson we are going to learn about the escape characters in the strings.
+splitString = "This string has been\nsplit over \nseveral \ntimes."
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+# Below example shows how to escape single quotes or double quotes in a string using the backslash.
+print("The pet shop owner said, \"No, No 'e 's... uh, He's resting\". ")
+
+# Or we can print the above line like this.
+
+print('The pet shop owner said, "No, No \'e \'s... uh, He\'s resting." ')
+# We can also use the triple quotes like this which will not require us to use the backslash.
+print("""The pet shop owner said, "No, No 'e 's... uh, He's resting". """)
+
