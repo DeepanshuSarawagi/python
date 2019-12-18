a5480861034f10e6e3164951d7d940d715f5b4b1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-18 18:55:03 +0530

    Learn about the operator precedence

--- Data Types/numericOperators.py
+++ Data Types/numericOperators.py
@@ -9,3 +9,10 @@ print(a * b)
 print(a / b)
 print(a // b)
 print(a % b)
+
+# We will learn about the operator precedence in the following example.
+
+print(a + b / 3 - 4 * 12)  # This should evaluate to -35.0 as per the BODMAS rule. If you have got it 12, you are wrong.
+print(a + (b/3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)  # This will evaluate to 12.0.
+print(a / (b * a) / b)
