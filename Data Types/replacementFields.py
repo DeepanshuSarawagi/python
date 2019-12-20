2bd920d360aa3a3a88e59338eeb4b1d62cecbba6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-20 17:40:00 +0530

    Learn about string replacement fields

---
+++ Data Types/replacementFields.py
@@ -0,0 +1,7 @@
+# In this lesson we are going to learn about string replacement fields.
+# It is possible to concatinate a number with string by converting the int data type to string using str function.
+
+age = 27
+print("My age is " + str(age) + " years.")
+
+# Hence str() function, we can coerce an integer to a string.
