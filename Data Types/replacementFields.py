e112cab3a49fc927d894b1b2bd875ebc8e92f13a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-20 17:52:29 +0530

    Learn more about multiple replacement fields in string formatting

--- Data Types/replacementFields.py
+++ Data Types/replacementFields.py
@@ -10,3 +10,7 @@ print("My age is " + str(age) + " years.")
 # with string formatting. Refer to code in line 11.
 print("My age is {0} years.".format(age))
 
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}.".
+      format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"))
+
+
