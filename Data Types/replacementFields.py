84beac6795c10c51caafee0e2da03a7c6764aff1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-21 23:46:08 +0530

    Learn about f-strings in Python

--- Data Types/replacementFields.py
+++ Data Types/replacementFields.py
@@ -1,8 +1,10 @@
 # In this lesson we are going to learn about string replacement fields.
-# It is possible to concatinate a number with string by converting the int data type to string using str function.
+# It is possible to concatenate a number with string by converting the int data type to string using str function.
 
+name =  "Deepanshu"
 age = 27
 print("My age is " + str(age) + " years.")
+print(name + " is " + str(age) + " years old.")
 
 # Hence str() function, we can coerce an integer to a string.
 # However, coercing every integer to a string could become tedious. Hence, Python 3 provides a better way of formatting
@@ -18,3 +20,8 @@ print("Jan: {2}, Feb: {0}, Mar: {2}, Apr: {1}, May: {2}, Jun: {1}, Jul: {2}, Aug
       .format(28, 30, 31))
 
 
+# Let's learn about f-strings
+# code on line 6 can also be written like this.
+
+print(name + f" is {age} years old. ")
+# code on line 26 is example of f-strings in python which is used in version 3.6 and above.
