10eacfda2ee756f317a5fb56233f39979c802c85
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-21 23:36:27 +0530

    Learn about precision field

--- Data Types/stringFormatting.py
+++ Data Types/stringFormatting.py
@@ -14,3 +14,25 @@ for i in range(1,13):
     print("No: {0:2} squared is {1:<3} and cubed is {2:<4}".format(i, i ** 2, i ** 3))
 
 # The less than symbol left aligns the characters in the output.
+print()
+
+# Learn how to center align the characters using the caret symbol
+
+for i in range(1,13):
+    print("No: {0:2} squared is {1:<3} and cubed is {2:^4}".format(i, i ** 2, i ** 3))
+
+print()
+# we are going to learn about the precision fields in python. Let's try to understand this by running the below code.
+# We will be calculating the value of pi and get the output in floating point value. The maximum precision fields python
+# can support is between 51 and 53.
+
+print("The value of pi is approximately {0:12}".format(22 / 7))
+print("The value of pi is approximately {0:12f}".format(22 / 7))
+print("The value of pi is approximately {0:12.50f}".format(22 / 7))
+print("The value of pi is approximately {0:52.50f}".format(22 / 7))
+print("The value of pi is approximately {0:62.50f}".format(22 / 7))
+print("The value of pi is approximately {0:<72.50f}".format(22 / 7))
+print("The value of pi is approximately {0:<72.54f}".format(22 / 7))
+
+# As you can compare the output after executing the code in line 34 and 35, python is able to get the value of pi upto
+# 51 places after the decimal point (precision value)
