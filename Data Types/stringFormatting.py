47f3a5fa0701b47d743acd19a7432b82335bdcb4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-21 23:11:48 +0530

    Learn about string formatting using replacement fields

---
+++ Data Types/stringFormatting.py
@@ -0,0 +1,4 @@
+# In this lesson we are going to learn more about the string formatting using replacement fields.
+
+for i in range(1,13):
+    print("No: {0} squared is {1} and cubed is {2}".format(i, i ** 2, i ** 3))
