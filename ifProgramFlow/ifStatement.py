84180f0d430884446c06cd775adb5d1a22cbeaac
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-22 08:26:04 +0530

    learn about if else statement

--- ifProgramFlow/ifStatement.py
+++ ifProgramFlow/ifStatement.py
@@ -11,3 +11,23 @@ if age >= 18:
     print("You are eligible to cast your vote, {}.".format(name))
 else:
     print("Please come back after {} years, {}".format(18 - age, name))
+
+print()
+
+guess = int(input("Please guess a number between 1 to 10: "))
+if guess < 5:
+    print("Please guess a higher number")
+    guess = int(input())
+    if guess == 5:
+        print("Well done, you guessed it correctly")
+    else:
+        print("Sorry you got it wrong this time")
+elif guess > 5:
+    print("Please guess a lower number")
+    guess = int(input())
+    if guess == 5:
+        print("Well done, you guessed it correctly")
+    else:
+        print("Sorry you got it wrong this time")
+else:
+    print("Well done, you got it right first time")
