ca380a71e67f6a143568c5a2e1fbd38978f77f60
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-22 08:05:11 +0530

    Create new project for learning if else statement

---
+++ ifProgramFlow/ifStatement.py
@@ -0,0 +1,6 @@
+__author__ = 'Dev'
+name = input("Please enter your name: ")
+# If you want to change the input data type to a certain type, then add the respective data type and assign it to the
+# variable.
+age = int(input("How old are you, {0}? ".format(name)))
+print(age)
