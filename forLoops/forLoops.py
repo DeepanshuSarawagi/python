0f117916e3746d3692a63929fb4c2e03d6bf9589
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-22 09:46:12 +0530

    for loops

--- forLoops/forLoops.py
+++ forLoops/forLoops.py
@@ -1,2 +1,7 @@
 for i in range(1,20):
     print(f"i is now {i}.")
+
+number = "9,223,456,865,207,590,007"
+for i in range(0, len(number)):
+    if number[i] in "0123456789":
+        print(number[i])
