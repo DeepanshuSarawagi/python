512da0a29282515500f5e50b55e4a36bbb7e694c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-22 09:50:20 +0530

    Learn how to print in same line using end=''

--- forLoops/forLoops.py
+++ forLoops/forLoops.py
@@ -4,4 +4,4 @@ for i in range(1,20):
 number = "9,223,456,865,207,590,007"
 for i in range(0, len(number)):
     if number[i] in "0123456789":
-        print(number[i])
+        print(number[i], end='')
