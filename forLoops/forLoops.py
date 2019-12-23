99492fa2414773a78f8f5b7a9383347a6738391c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-23 10:47:13 +0530

    introducing steps in for loops

--- forLoops/forLoops.py
+++ forLoops/forLoops.py
@@ -16,3 +16,11 @@ print(newNumber)
 print()
 for state in ["not pinin", "no more", "bereft of life", "a stiff"]:
     print("This parrot is " + state)
+
+# introducing steps in for loops. In the below example, the third number in range is step; which means python is going
+# to skip the number in sequence. Lets run the code below to understand more.
+
+for a in range(0, 100, 5):
+    print(f"a is now {a}")
+
+# As you can see, after executing the code, python has skipped every 5th number in the range from 0 to 100.
