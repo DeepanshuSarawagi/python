e8c6222da9638f00011531a8aaa39e2ed0a7e075
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2019-12-23 10:56:07 +0530

    nesting for loops inside for loop

--- forLoops/forLoops.py
+++ forLoops/forLoops.py
@@ -18,9 +18,16 @@ for state in ["not pinin", "no more", "bereft of life", "a stiff"]:
     print("This parrot is " + state)
 
 # introducing steps in for loops. In the below example, the third number in range is step; which means python is going
-# to skip the number in sequence. Lets run the code below to understand more.
+# to skip to the number in sequence. Lets run the code below to understand more.
 
 for a in range(0, 100, 5):
     print(f"a is now {a}")
 
-# As you can see, after executing the code, python has skipped every 5th number in the range from 0 to 100.
+# As you can see, after executing the code, python has skipped to every 5th number in the range from 0 to 100.
+
+# nesting for loops inside a for loop.
+print()
+for i in range(1, 16):
+    for j in range(1, 11):
+        print("{0:2} times {1:2} is {2:3}".format(i, j, i*j))
+    print(50*"=")
