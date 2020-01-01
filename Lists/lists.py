bac35207d6d6713705a7d7c323be959c8f6b5679
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-01 13:36:15 +0530

    Create a simple list

---
+++ Lists/lists.py
@@ -0,0 +1,5 @@
+# In this lesson we are going to learn about lists in Python.
+
+parrot_list = ["Not pinin'", "no more", "a stiff", "bereft of life"]
+for state in parrot_list:
+    print(f"This parrot is {state}", end="\t")
