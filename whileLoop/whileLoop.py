94b9fc50ecd2c3521fd5fd57a6797873c78b61ed
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-01 11:57:58 +0530

    understand while loops

--- whileLoop/whileLoop.py
+++ whileLoop/whileLoop.py
@@ -5,3 +5,10 @@ i = 0
 while i < 10:
     print(f"i is now {i}")
     i += 1
+
+available_exit =["east", "west", "south"]
+chosen_exit = ""
+while chosen_exit not in available_exit:
+    chosen_exit = input("Please enter a direction: ")
+
+print("Glad that you got out of here")
