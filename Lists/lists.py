7c59e9037774a6861bc7a18f04fbb21dc44261ae
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-01 13:39:37 +0530

    Understand how to append in a list

--- Lists/lists.py
+++ Lists/lists.py
@@ -3,3 +3,11 @@
 parrot_list = ["Not pinin'", "no more", "a stiff", "bereft of life"]
 for state in parrot_list:
     print(f"This parrot is {state}", end="\t")
+# below code will show how to append an item to a list
+
+parrot_list.append("A Norwegian Blue")
+print()
+print()
+
+for state in parrot_list:
+    print(f"This parrot is {state}")
