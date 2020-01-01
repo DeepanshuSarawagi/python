7710eb6f6e3c2283f2b2e2e961dfd2aa9f80e192
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-01 12:52:39 +0530

    while loop challenge

--- whileLoop/whileLoopChallenge.py
+++ whileLoop/whileLoopChallenge.py
@@ -1,14 +1,22 @@
 import random
-highest = 10
+highest = 100
 guess = 0
+no_of_guess = 0
 answer = random.randint(1, highest)
 print(f"Please guess a number between 1 and {highest}: ")
 
 while guess != answer:
     guess = int(input())
+    no_of_guess += 1
+    if guess == 0:
+        print("You cannot guess 0")
+        break
+    if no_of_guess > 20 and guess != answer:
+        print("You cannot have more than 20 guesses")
+        break
     if guess < answer:
         print("Please guess higher")
     elif guess > answer:
         print("Please guess lower")
     else:
-        print("You have got it right this time")
+        print(f"You have got it right in {no_of_guess} guesses")
