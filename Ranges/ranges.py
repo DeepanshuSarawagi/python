446f06aa31f5ae5ccd8a801e6990c70401320573
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-03 10:28:29 +0530

    Understanding index of a sequence

--- Ranges/ranges.py
+++ Ranges/ranges.py
@@ -12,3 +12,11 @@ print(odd)
 
 """We can also define the start and end value of ranges. In the above example we have used step of 2 which means 
 it will skip every consecutive element in the defined range between 0 and 10 for even and 1 to 10 for odd."""
+
+myString = "abcdefghijklmnopqrstuvwxyz"
+print(myString.index('i'))
+print(myString[4])
+
+largeNumber = range(1, 10000000, 3)
+print(largeNumber)
+print(largeNumber[5550])
