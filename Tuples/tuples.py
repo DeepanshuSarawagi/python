f3e0b5bfd5d99db2a7b3b431af9030cccce0631f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-03 11:53:56 +0530

    Tuples are immutable

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -11,3 +11,23 @@ print(("Deepasnhu", "Sarawagi", 27, "Male"))  # This is a tuple, since data is e
                                               # function.
 print(data[1])
 print(data[2])
+
+"""Lets create some more tuples to understand more"""
+metallica = ("Ride the lightning", "Metallica", 1984)
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+# metallica[0] = "Master of Puppets"
+"""Tuples are immutable which means once created, they cannot be changed. But thats not the case with lists.
+Look at the below example to understand more."""
+
+metallica2 = ["Ride the lightning, Metallica", 1984]
+print(metallica2)
+print()
+metallica2[0] = "Master of Puppets"
+print(metallica2)
+
+"""As you can see from line 26 to 30, we created a list called metallica2 and changed the first item in the list and
+it worked, however, changing the items in a tuple is not possible since they are immutable."""
