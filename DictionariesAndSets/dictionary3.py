4554887389802cafbb5793659eed733727bc9569
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-07 21:02:44 +0530

    Learn dictionary within dictionary

---
+++ DictionariesAndSets/dictionary3.py
@@ -0,0 +1,14 @@
+fruits = {"Orange": "A sweet orange citrus fruit, usually good for skin",
+          "Apple": "Good for health if eaten once a day",
+          "Lime": "A green sour/sweet citrus fruit",
+          "Lemon": "A yellow sour citrus fruit, usually good with Vodka"}
+
+print(fruits)
+print(fruits.keys())
+print(fruits["Lemon"])
+
+veg = {"cabbage": {1: "Good for health"},
+       "carrot": {2: "Good for eye"}}
+print(veg["carrot"][2])  # Extracting the value from dictionary within dictionary
+
+
