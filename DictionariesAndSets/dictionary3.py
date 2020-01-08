f5eb3d70db02f027167590098c9b60dae84fe416
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-08 19:58:22 +0530

    Make changes as per the challenge on Udemy

--- DictionariesAndSets/dictionary3.py
+++ DictionariesAndSets/dictionary3.py
@@ -33,3 +33,58 @@ print()
 
 for f in sorted(nice_veg.keys()):
     print(f + " - " + nice_veg[f])
+
+"""Continued dictionary challenge as per dictionary2.py We need to modify the program in such a way that users can 
+enter the location name instead of the exits and the program should work."""
+
+locations = {0: "You are sitting in front of the computer learning Python",
+             1: "You are standing at the end of the road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for small stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in the forest"}
+# We have created a dictionary containing the locations and its corresponding keys.
+# Now we will create a list of dictionaries for available exits.
+
+exits = {0: {"Q": 0},
+         1: {"W": 2, "2": 2, "E": 3, "3": 3, "N": 5, "5": 5, "S": 4, "4": 4, "Q": 0},
+         2: {"N": 5, "5": 5, "Q": 0},
+         3: {"W": 1, "1": 1, "Q": 0},
+         4: {"N": 1, "1": 1, "W": 2, "2": 2, "Q": 0},
+         5: {"W": 2, "2": 2, "S": 1, "1": 1, "Q": 0}}
+
+vocabulary = {"QUIT": "Q",
+              "NORTH": "N",
+              "SOUTH": "S",
+              "EAST": "E",
+              "WEST": "W",
+              "ROAD": "1",
+              "HILL": "2",
+              "BUILDING": "3",
+              "VALLEY": "4",
+              "FOREST": "5"}
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+    # for direction in exits[loc].keys():
+    #     print(direction)
+    #     availableExits += direction + ", "
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    chosenDirection = input("Available exits are " + availableExits + ": ").upper()
+    # We are going to create a for loop to check if the user input is in vocabulary
+    if len(chosenDirection) > 1:
+        words = chosenDirection.split()
+        for word in words:
+            if word in vocabulary:
+                chosenDirection = vocabulary[word]
+
+    if chosenDirection in exits[loc]:
+        loc = exits[loc][chosenDirection]
+        print(loc)
+    else:
+        print("You cannot go in that direction")
