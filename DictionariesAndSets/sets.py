bb0a885126d732322c0e424bcd69bbfbc64e447e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-10 19:58:27 +0530

    Create new python file to learn about sets

---
+++ DictionariesAndSets/sets.py
@@ -0,0 +1,5 @@
+# Just like dictionaries, even sets can be defined in the curly parenthesis.
+# Let's create a set and assign to be variable farm_animals
+
+farm_animals = {"sheep", "cow", "hen"}
+print(farm_animals)
