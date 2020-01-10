1671f734b4e991c118f81dc786193c746751ff34
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-10 20:07:07 +0530

    Learn how to define a set

--- DictionariesAndSets/sets.py
+++ DictionariesAndSets/sets.py
@@ -3,3 +3,17 @@
 
 farm_animals = {"sheep", "cow", "hen"}
 print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+print("=" * 50)
+
+wild_animals = set(["lion", "tiger", "panther", "wild elephant", "cheetah", "leopard"])
+"""Ignore the above warning, we are just learning different methods to define a set."""
+# wild_animals = set(wild_animals)
+print(wild_animals)
+for animal in wild_animals:
+    print(animal)
+wild_animals = sorted(wild_animals)  # we can also sort a set and it will return the list of sorted items in it
+print("=" * 50)
+print(wild_animals)
