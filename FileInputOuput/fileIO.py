3335377fdb631b92952d29a27c8569945b151f46
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-11 21:58:30 +0530

    how to search a string in file and print only those lines

--- FileInputOuput/fileIO.py
+++ FileInputOuput/fileIO.py
@@ -3,3 +3,11 @@ jabber = open("sample.txt", 'r')
 for lines in jabber:
     print(lines)
 jabber.close()
+
+# There is a better way of performing file I/O operations using the "with" statement. Advantage of using with statement
+# is that we need not explicitly specify a file to close.
+
+with open("sample.txt", 'r') as f:
+    for lines in f:
+        if "JAB" in lines.upper():
+            print(lines, end='')
