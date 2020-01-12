fa85dd9e8d450df2c62fc21a562d6a470110e879
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-12 19:38:31 +0530

    How to retrieve data from the pickled file

--- FileInputOuput/Pickles/pickling.py
+++ FileInputOuput/Pickles/pickling.py
@@ -18,3 +18,26 @@ with open("imelda.pickle", 'bw') as imelda_file:  # Write to the file with exten
 with open("imelda.pickle", 'br') as imelda_pickled:
     imelda2 = pickle.load(imelda_pickled)
 print(imelda2)
+
+# write some more data in the binary file using pickle.
+
+even = list(range(0, 20, 2))
+odd = list(range(1, 20, 2))
+
+with open("imelda.pickle", "ba") as imelda_file:
+    pickle.dump(even, imelda_file)
+    pickle.dump(odd, imelda_file)
+    pickle.dump(2998302, imelda_file)
+
+with open("imelda.pickle", 'br') as imelda_pickled:
+    even_list = pickle.load(imelda_pickled)
+    odd_list = pickle.load(imelda_pickled)
+    x = pickle.load(imelda_pickled)
+
+for i in even_list:
+    print(i)
+
+for j in odd_list:
+    print(j)
+
+print(x)
