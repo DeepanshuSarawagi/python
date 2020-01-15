4dc5c7bfcaf993eeb8f51fea19e87a09761349ca
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-15 18:59:47 +0530

    Create new python file to learn on time module in python

---
+++ ModulesAndFunctions/dateAndTime/datecalc.py
@@ -0,0 +1,7 @@
+import time
+
+print(time.gmtime(0))  # This will print the epoch time of this system which is usually January 1, 1970
+
+print(time.localtime())  # This will print the local time
+
+print(time.time())  # This will print the time in seconds since epoch time
