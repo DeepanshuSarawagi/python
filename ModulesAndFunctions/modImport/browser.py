3ab4f80cb02ab17f8c4d08a89ce08a2181a39a2b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-15 18:37:12 +0530

    webbrowser module

--- ModulesAndFunctions/modImport/browser.py
+++ ModulesAndFunctions/modImport/browser.py
@@ -3,3 +3,8 @@ import webbrowser
 webbrowser.get(using=None)
 
 webbrowser.open("https://www.python.org")
+
+"""Functions with names arguments. sep= and end= are the named arguments defined in the print function"""
+
+for i in range(10):
+    print(1, 2, 3, 4, 5, 6, 7, 8, 9, sep='; ', end='\t')
