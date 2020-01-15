21974eed1875e31322b5dcba4bcb9e9c6e819b85
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-15 19:16:43 +0530

    learn how to print local date and time

--- ModulesAndFunctions/dateAndTime/datecalc.py
+++ ModulesAndFunctions/dateAndTime/datecalc.py
@@ -5,3 +5,11 @@ print(time.gmtime(0))  # This will print the epoch time of this system which is 
 print(time.localtime())  # This will print the local time
 
 print(time.time())  # This will print the time in seconds since epoch time
+
+time_here = time.localtime()
+
+print(time_here)
+for i in time_here:
+    print(i)
+
+print(time_here[0])
