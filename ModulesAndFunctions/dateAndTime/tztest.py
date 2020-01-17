d9d059b78767c0d072c57e8a779b4b418c721cb5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-17 19:44:34 +0530

    Learn about pytz module and how to use it get different time zones

---
+++ ModulesAndFunctions/dateAndTime/tztest.py
@@ -0,0 +1,20 @@
+import pytz
+import datetime
+
+country = "Europe/Moscow"
+
+tz_to_display = pytz.timezone(country)
+local_time = datetime.datetime.now(tz=tz_to_display)
+print(f"The time in country {country} is {local_time}")
+print(f"The UTC time is {datetime.datetime.utcnow()}")
+
+for x in pytz.all_timezones:
+    print(x)                   # This will print all the timezones which pytz.timezone() method will accept
+
+for x in pytz.country_names:
+    print(x + ": " + pytz.country_names[x])  # This will print all the country codes and country names
+
+print("=" * 50)
+
+for x in sorted(pytz.country_names):
+    print(f"{x}: {pytz.country_names[x]}: {pytz.country_timezones.get(x)}")
