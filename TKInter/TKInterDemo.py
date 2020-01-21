08992fd4eb36129cc57b84af20c60031ce4c6e5b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-21 11:43:10 +0530

    Create label and canvas inside mainWindow using tkinter

--- TKInter/TKInterDemo.py
+++ TKInter/TKInterDemo.py
@@ -8,4 +8,11 @@ print(tkinter.TclVersion)
 mainWindow = tkinter.Tk()
 mainWindow.title("Hello World")
 mainWindow.geometry("640x480")
+
+label = tkinter.Label(mainWindow, text="Hello World")
+label.pack(side="top")
+
+canvas = tkinter.Canvas(mainWindow, relief="raised", borderwidth=2)
+canvas.pack(side="left")
+
 mainWindow.mainloop()
