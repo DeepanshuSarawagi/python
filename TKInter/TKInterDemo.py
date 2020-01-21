8462fb5b4f6d851e433543b190a989648b439ffe
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-21 11:33:18 +0530

    Create a basic window using TKInter

--- TKInter/TKInterDemo.py
+++ TKInter/TKInterDemo.py
@@ -3,4 +3,9 @@ import tkinter
 print(tkinter.TkVersion)
 print(tkinter.TclVersion)
 
-tkinter._test()
+# tkinter._test()
+
+mainWindow = tkinter.Tk()
+mainWindow.title("Hello World")
+mainWindow.geometry("640x480")
+mainWindow.mainloop()
