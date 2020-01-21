4f4a2533444b03bc9d125263f12e813599bfd9da
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-21 11:52:56 +0530

    Adding buttons and understand about anchors in tkinter

--- TKInter/TKInterDemo.py
+++ TKInter/TKInterDemo.py
@@ -13,6 +13,14 @@ label = tkinter.Label(mainWindow, text="Hello World")
 label.pack(side="top")
 
 canvas = tkinter.Canvas(mainWindow, relief="raised", borderwidth=2)
-canvas.pack(side="left", fill=tkinter.BOTH, expand=True)
+canvas.pack(side="left")
+# canvas.pack(side="left", fill=tkinter.BOTH, expand=True)
 
+button1 = tkinter.Button(mainWindow, text="button1")
+button2 = tkinter.Button(mainWindow, text="button2")
+button3 = tkinter.Button(mainWindow, text="button3")
+
+button1.pack(side="left", anchor="n")
+button2.pack(side="left", anchor="e")
+button3.pack(side="left", anchor="s")
 mainWindow.mainloop()
