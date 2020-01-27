6eb5f275884aa14e633a21dbb350bfecdb2d5b32
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-27 12:15:44 +0530

    Create simple function

--- ModulesAndFunctions/Functions/functions.py
+++ ModulesAndFunctions/Functions/functions.py
@@ -1,6 +1,10 @@
 def center_text(text):
-    left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    if text == 'quit':
+        print("You have decided to quit")
+    else:
+        left_margin = (80 - len(text)) // 2
+        print(" " * left_margin, text)
+
 
 text = input("Please enter a text: ")
 center_text(text)
