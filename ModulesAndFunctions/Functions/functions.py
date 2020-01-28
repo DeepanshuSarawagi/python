e9009a539815949ccec38a95cfc7021a87387881
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-28 11:34:21 +0530

    learn basics of functions

--- ModulesAndFunctions/Functions/functions.py
+++ ModulesAndFunctions/Functions/functions.py
@@ -11,17 +11,25 @@ text = input("Please enter a text: ")
 center_text(text)
 
 
-def centered_text(*args, sep=' ', end='\n', file=None, flush=False):
+# def centered_text(*args, sep=' ', end='\n', file=None, flush=False):
+#     text = ""
+#     for arg in args:
+#         text += str(arg) + sep
+#     text = text.strip(":")
+#     left_margin = (80 - len(text)) // 2
+#     print(" " * left_margin, text, end=end, file=file, flush=flush)
+# commenting out above code for reference
+def centered_text(*args, sep=' '):
     text = ""
     for arg in args:
         text += str(arg) + sep
     text = text.strip(":")
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    return " " * left_margin + text
 
 
-with open("centered.txt", mode='w') as centered_file:
-    centered_text("spam", 1, 2, "eggs", "number", sep=":", file=centered_file)
-    centered_text("spam and spam", file=centered_file)
-    centered_text("spam, spam and spam", file=centered_file)
-    centered_text("text without spam", file=centered_file)
+with open("menu.txt", mode='w') as centered_file:
+    print(centered_text("spam", 1, 2, "eggs", "number", sep=":"), file=centered_file)
+    print(centered_text("spam and spam"), file=centered_file)
+    print(centered_text("spam, spam and spam"), file=centered_file)
+    print(centered_text("text without spam"), file=centered_file)
