d775002d08ca89109285c096ffa2bf708696ae36
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-30 11:01:55 +0530

    Print local variables of a function

--- ModulesAndFunctions/Functions/parabolaFunction.py
+++ ModulesAndFunctions/Functions/parabolaFunction.py
@@ -14,6 +14,7 @@ def draw_axes(canvas):
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
     canvas.create_line(-x_origin, 0, x_origin, 0, fill='black')
     canvas.create_line(0, y_origin, 0, -y_origin, fill='black')
+    print(locals())
 
 
 def plot(canvas, x, y):
@@ -29,6 +30,7 @@ canvas.grid(row=0, column=0)
 canvas2 = tkinter.Canvas(mainWindow, width=320, height=480)
 canvas2.grid(row=0, column=1)
 
+print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
 draw_axes(canvas2)
 
