1c1eabac948abe23f68b22c46cb3f28cebbd4c35
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-30 11:27:59 +0530

    Learn about scope of variables in functions

--- ModulesAndFunctions/Functions/parabolaFunction.py
+++ ModulesAndFunctions/Functions/parabolaFunction.py
@@ -1,41 +1,37 @@
 import tkinter
 
 
-def parabola(x):
-    y = x*x / 100
-    return y
+def parabola(page, size):
+    for x in range(-size, size):
+        y = x*x / size
+        plot(page, x, y)
 
 
 # draw axes in the canvas
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill='black')
-    canvas.create_line(0, y_origin, 0, -y_origin, fill='black')
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    page.create_line(-x_origin, 0, x_origin, 0, fill='black')
+    page.create_line(0, y_origin, 0, -y_origin, fill='black')
     print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill='blue')
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill='blue')
 
 
 mainWindow = tkinter.Tk()
 mainWindow.title('Parabola')
 mainWindow.geometry('640x480')
 
-canvas = tkinter.Canvas(mainWindow, width=320, height=480)
+canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
-canvas2 = tkinter.Canvas(mainWindow, width=320, height=480)
-canvas2.grid(row=0, column=1)
-
-print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
-draw_axes(canvas2)
 
-for x in range(-100, 100):
-    y = parabola(x)
-    plot(canvas, x, -y)
+parabola(canvas, 100)
+parabola(canvas, 200)
+
 
 mainWindow.mainloop()
