7030d23f6dfb0d98db477f44c7eaa499624133c2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-01-30 10:53:49 +0530

    Create one more canvas

--- ModulesAndFunctions/Functions/parabolaFunction.py
+++ ModulesAndFunctions/Functions/parabolaFunction.py
@@ -24,10 +24,13 @@ mainWindow = tkinter.Tk()
 mainWindow.title('Parabola')
 mainWindow.geometry('640x480')
 
-canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas = tkinter.Canvas(mainWindow, width=320, height=480)
 canvas.grid(row=0, column=0)
+canvas2 = tkinter.Canvas(mainWindow, width=320, height=480)
+canvas2.grid(row=0, column=1)
 
 draw_axes(canvas)
+draw_axes(canvas2)
 
 for x in range(-100, 100):
     y = parabola(x)
