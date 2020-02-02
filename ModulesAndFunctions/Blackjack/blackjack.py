60e3ae5e73f12f29d2e28b3f48709de7987f3d2e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-02 22:37:38 +0530

    Create dealer score label

--- ModulesAndFunctions/Blackjack/blackjack.py
+++ ModulesAndFunctions/Blackjack/blackjack.py
@@ -15,4 +15,12 @@ result.grid(row=0, column=0, columnspan=3)
 card_frame = tkinter.Frame(mainWindow, relief='sunken', borderwidth=2, background='green')
 card_frame.grid(row=1, column=0, columnspan=3, rowspan=2, sticky='ew')
 
+# dealer score label
+
+dealer_score_label = tkinter.IntVar()
+tkinter.Label(card_frame, text='dealer', background='green', fg='white').grid(row=0, column=0)
+tkinter.Label(card_frame, text=dealer_score_label, background='green', fg='white').grid(row=1, column=0)
+
+# embedded frames to hold the cards
+
 mainWindow.mainloop()
