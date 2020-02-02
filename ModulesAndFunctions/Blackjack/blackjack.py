dc6cd568884b97eeb1c6a4a70f889a849a12c9e1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-02 23:23:16 +0530

    Create dealer and player button

--- ModulesAndFunctions/Blackjack/blackjack.py
+++ ModulesAndFunctions/Blackjack/blackjack.py
@@ -30,4 +30,18 @@ player_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text='player', background='green', fg='white').grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background='green', fg='white').grid(row=3, column=0)
 
+# embedded frames to hold the cards
+player_card_frame = tkinter.Frame(card_frame, background='green')
+player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+
+# Add buttons for dealer and player
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, sticky='w', columnspan=3)
+
+dealer_button = tkinter.Button(button_frame, text='Dealer')
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text='Player')
+player_button.grid(row=0, column=1)
+
 mainWindow.mainloop()
