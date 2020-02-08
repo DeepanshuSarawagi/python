a5b9d22976ee2a9467ff2d54d034bf126abcf98e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-08 20:38:40 +0530

    Learn how to calculate fibonacci series

--- ModulesAndFunctions/ScopesAndRecursive/recursiveFunction.py
+++ ModulesAndFunctions/ScopesAndRecursive/recursiveFunction.py
@@ -23,3 +23,37 @@ def factorial(n):
 
 for i in range(50):
     print(i, factorial(i))
+
+"""Write a program recursively to calculate fibonacci series"""
+
+
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def fibonacci(n):
+    global result
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        n_minus1 = 1
+        n_minus2 = 0
+        for f in range(1, n):
+            result = n_minus2 + n_minus1
+            # print(f"n_minus2 is now {n_minus2}")  # just to understand the code
+            # print(f"n_minus1 is now {n_minus1}")
+            # print(f"result is now {result}")
+            n_minus2 = n_minus1
+            # print(f"n_minus2 is now {n_minus2}")
+            n_minus1 = result
+            # print(f"n_minus1 is now {n_minus1}")
+    return result
+
+
+for i in range(36):
+    print(i, fibonacci(i))
