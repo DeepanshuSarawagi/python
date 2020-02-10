429339f9917bd71a940435a9795d7f7bc796202a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-10 21:23:35 +0530

    Create a function to list the directories, files and sub-directories

--- ModulesAndFunctions/ScopesAndRecursive/filesAndDirectories.py
+++ ModulesAndFunctions/ScopesAndRecursive/filesAndDirectories.py
@@ -1,5 +1,15 @@
 import os
 
+
+def list_directories(s):
+    tab_stop = 0
+    if os.path.exists(s):
+        print(f"Directory listings of {s}")
+        dir_list(s)
+    else:
+        print(f"The following path {s} doesn't exist")
+
+
 listing = os.walk('/Users/deepanshusarawagi/Desktop/Learning/python')
 for root, directories, files in listing:
     print(root)
