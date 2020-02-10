e6e265960e518fd052854abe06f2ea05b7a88dde
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-10 21:46:10 +0530

    Learn how to use nonlocal keyword

--- ModulesAndFunctions/ScopesAndRecursive/filesAndDirectories.py
+++ ModulesAndFunctions/ScopesAndRecursive/filesAndDirectories.py
@@ -2,6 +2,20 @@ import os
 
 
 def list_directories(s):
+
+    def dir_list(d):
+        nonlocal tab_stop
+        files = os.listdir(d)
+        for f in files:
+            current_dir = os.path.join(d, f)
+            if os.path.isdir(current_dir):
+                print("\t" * tab_stop + "Directory " + f)
+                tab_stop += 1
+                dir_list(current_dir)
+                tab_stop -= 1
+            else:
+                print("\t" * tab_stop + f)
+
     tab_stop = 0
     if os.path.exists(s):
         print(f"Directory listings of {s}")
@@ -10,10 +24,12 @@ def list_directories(s):
         print(f"The following path {s} doesn't exist")
 
 
-listing = os.walk('/Users/deepanshusarawagi/Desktop/Learning/python')
-for root, directories, files in listing:
-    print(root)
-    for d in directories:
-        print(d)
-    for file in files:
-        print(file)
+list_directories('/Users/deepanshusarawagi/Desktop/Learning/python')
+
+# listing = os.walk('/Users/deepanshusarawagi/Desktop/Learning/python')
+# for root, directories, files in listing:
+#     print(root)
+#     for d in directories:
+#         print(d)
+#     for file in files:
+#         print(file)
