87f56b46fdcc3b6e153d38a1b0ee55b85c15d427
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-11 17:03:26 +0530

    create a program to understand local variable scope in nested functions

---
+++ ModulesAndFunctions/ScopesAndRecursive/locals.py
@@ -0,0 +1,21 @@
+def spam1():
+
+    def spam2():
+
+        def spam3():
+            z = " even more spam"
+            print(f"locals of spam3 are {locals()}")
+            return z
+
+        y = " more spam"
+        y += spam3()
+        print(f"locals of spam2 are {locals()}")
+        return y
+
+    x = "spam "
+    x += spam2()
+    print(f"locals of spam1 are {locals()}")
+    return x
+
+
+print(spam1())
