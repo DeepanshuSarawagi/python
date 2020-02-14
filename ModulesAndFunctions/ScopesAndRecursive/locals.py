8ef4f669ea1dcbec56dfc9bb285d78fd195bad7a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-14 22:30:37 +0530

    Create a program to understand free variables in functions

--- ModulesAndFunctions/ScopesAndRecursive/locals.py
+++ ModulesAndFunctions/ScopesAndRecursive/locals.py
@@ -19,3 +19,27 @@ def spam1():
 
 
 print(spam1())
+
+
+def spam1():
+
+    def spam2():
+
+        def spam3():
+            z = ' even '
+            z += y
+            print(f"locals of spam3 are {locals()}")
+            return z
+
+        y = ' more' + x
+        y += spam3()
+        print(f"locals of spam2 are {locals()}")
+        return y
+
+    x = " spam"
+    x += spam2()
+    print(f"locals of spam1 are {locals()}")
+    return x
+
+
+print(spam1())
