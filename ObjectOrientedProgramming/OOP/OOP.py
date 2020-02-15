57bef776280ef1c9772af105f09604a1be936871
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-15 22:29:27 +0530

    Understand the basic concept of OOP
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- ObjectOrientedProgramming/OOP/OOP.py
+++ ObjectOrientedProgramming/OOP/OOP.py
@@ -3,3 +3,29 @@ b = 4
 print(a + b)
 print(a.__add__(b))  # notice that both the code on line 3 and line 4 gives the same result since "+" operator on line 3
                      # calls the same builtin function __add__ as on line 4.
+
+# create your first class and its objects.
+
+
+class Kettle(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+
+kenwood = Kettle("Kenwood", 8.99)
+
+"""kenwood is the instance of the Kettle class and it shares the same characteristics defined in the Kettle class such
+ as make, price and on."""
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+print(hamilton.make)
+print(hamilton.price)
+print(hamilton.on)
