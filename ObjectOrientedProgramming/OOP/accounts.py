a4abc0803ce4bafbd81d6601eb88400b6069da86
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-16 15:54:00 +0530

    Learn mangling of objects in the class
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- ObjectOrientedProgramming/OOP/accounts.py
+++ ObjectOrientedProgramming/OOP/accounts.py
@@ -12,20 +12,21 @@ class Accounts:
 
     def __init__(self, name, balance):
         self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print(f"The account has been created for {self.name} and balance is {self.balance}")
+        self.__balance = balance
+        self.transaction_list = [(Accounts._current_time(), balance)]
+        print(f"The account has been created for {self.name} and balance is {self.__balance}")
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
         print(f"Amount {amount} has been deposited in the account")
         self.show_balance()
         self.transaction_list.append((Accounts._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
             self.transaction_list.append((Accounts._current_time(), -amount))
             print(f"Amount {amount} has been withdrawn from the account")
         else:
@@ -33,7 +34,7 @@ class Accounts:
         self.show_balance()
 
     def show_balance(self):
-        print(f"Balance in the account of {self.name} is {self.balance}")
+        print(f"Balance in the account of {self.name} is {self.__balance}")
 
     def show_transactions(self):
         for date, amount in self.transaction_list:
@@ -51,5 +52,17 @@ if __name__ == "__main__":
 
     deep.deposit(1000)
     deep.withdraw(500)
+    deep.deposit(5000)
+    deep.withdraw(1000)
     deep.show_transactions()
     # deep.show_balance()
+    rajat = Accounts("Rajat", 800)
+    rajat.deposit(100)
+    rajat.withdraw(50)
+    rajat.balance = 50
+    """to prevent this from happening on line 62, we need to use two underscores in data attributes of the methods in the class.
+    We have refactored the balance data attribute in the __init__ method to __balance. This process is called as
+    mangling."""
+    rajat.show_transactions()
+    rajat.show_balance()
+    print(rajat.__dict__)
