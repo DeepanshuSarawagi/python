7d81a0ae20944edd3f6781d1db9aefaee4eb507b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-16 14:09:41 +0530

    Create a new python file to learn more about classes
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

---
+++ ObjectOrientedProgramming/OOP/accounts.py
@@ -0,0 +1,20 @@
+class Accounts:
+    """A simple class for maintaining accounts"""
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        print(f"The account has been created for {self.name} and balance is {self.balance}")
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+        print(f"Amount {amount} has been deposited in the account and balance is {self.balance}")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            self.balance -= amount
+        print(f"Amount {amount} has been withdrawn from the account and balance is {self.balance}")
+
+    def show_balance(self):
+        print(f"Balance in the account is {self.balance}")
