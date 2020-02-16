159eb56deef09bbe9630ba449ad1a2d9f9330c71
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-02-16 17:14:02 +0530

    Learn how to use docstrings when creating classes
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- ObjectOrientedProgramming/OOP/song.py
+++ ObjectOrientedProgramming/OOP/song.py
@@ -14,11 +14,47 @@ class Song:
             title(str): Initialises the title attribute.
             artist(Artist): An object representing the song's creator.
             duration(Optional[int]): Initial value for the 'duration' attribute.
-                will default to zero if not specified
+                Will default to zero if not specified
         """
         self.title = title
         self.artist = artist
         self.duration = duration
 
 
+class Album:
+    """ Class to represent an Album using it's track list.
+    Attributes:
+        album_name(str): The name of the album.
+        year(int): The year album was released.
+        artist: (Artist): The artist responsible for the album.
+                If not specified, the artist will default to an artist with the name "various artists"
+        tracks (list[song]): list of the songs in the album.
+
+    Methods:
+        addSong: Used to add a new song to the album's track list.
+    """
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        if artist is None:
+            self.artist = Artist("Various Artists")
+        else:
+            self.artist = artist
+        self.tracks = []
+
+    def add_song(self, song, position=None):
+        """Adds a song to the track list
+        Args:
+            song (Song): A song to add.
+            position(optional[int]): If specified, song will be added to that position in the track list
+            - inserting it between other songs if necessary.
+            Otherwise, song will be added to the end of the list.
+        """
+        if position is None:
+            self.tracks.append(song)
+        else:
+            self.tracks.insert(position, song)
+
+
 help(Song.__init__)  # This will display the document of __init__ method of class Song
+print(Song.__doc__)  # Alternate method to print the documentation of class and it's objects if any.
