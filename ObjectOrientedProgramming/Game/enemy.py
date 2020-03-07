82f923e026b5f34fa35c261aeb0f865df821a108
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-07 19:48:04 +0530

    Create a new method in class Enemy to calculate the hit_points, remaining points and lives
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- ObjectOrientedProgramming/Game/enemy.py
+++ ObjectOrientedProgramming/Game/enemy.py
@@ -4,3 +4,11 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+
+    def take_damage(self, damage):
+        remaining_points = self.hit_points - damage
+        if remaining_points >= 0:
+            self.hit_points = remaining_points
+            print(f"I have {damage} points damage and have {self.hit_points} points left")
+        else:
+            self.lives -= 1
