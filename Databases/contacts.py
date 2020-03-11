b640207f7e1d569fd927efb01fe42ad9196c3999
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-11 10:51:38 +0530

    Learn how to use sqlite3 in Python

---
+++ Databases/contacts.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+db = sqlite3.connect("contacts.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts (name, phone, email) VALUES('Deepanshu', 654789, 'deepanshu@email.com')")
+db.execute("INSERT INTO contacts VALUES('Rajat', 987654, 'rajat@wmail.com')")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+for row in cursor:
+    print(row)
+
+db.close()
