ada5d979da22aee79da13d29fd79e6174b16b4e8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-12 10:16:14 +0530

    Make changes to learn about SQL Injection attack

--- Databases/challenge.py
+++ Databases/challenge.py
@@ -2,16 +2,19 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
-cursor = db.cursor()
+for row in db.execute("SELECT * FROM sqlite_master"):
+    print(row)
 
-cursor.execute("SELECT * FROM contacts")
-
-for name, phone, email in cursor:
-    print(name)
-    print(phone)
-    print(email)
-    print("*" * 40)
-
-cursor.close()
+# cursor = db.cursor()
+#
+# cursor.execute("SELECT * FROM sqlite_master")
+#
+# for name, phone, email in cursor:
+#     print(name)
+#     print(phone)
+#     print(email)
+#     print("*" * 40)
+#
+# cursor.close()
 
 db.close()
