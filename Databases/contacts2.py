ef98227dd38b74e1b764d17d77b5f80dae62783d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-12 09:37:54 +0530

    learn how to use cursor to update data and count how many rows affected

--- Databases/contacts2.py
+++ Databases/contacts2.py
@@ -1,7 +1,16 @@
 import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
-for row in db.execute("SELECT * FROM contacts"):
-    print(row)
+update_sql = "UPDATE contacts set email = 'update@update.com' WHERE contacts.phone = 654789"
+update_cursor = db.cursor()
+update_cursor.execute(update_sql)
+print(f"{update_cursor.rowcount} rows updated")
+update_cursor.close()
+
+for name, phone, email in db.execute("SELECT * FROM contacts"):
+    print(name)
+    print(phone)
+    print(email)
+    print("*" * 40)
 
 db.close()
