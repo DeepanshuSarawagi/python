ab38d221390ee6ad5652e0847a07b21fe8052539
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-12 10:27:10 +0530

    Learn how to avoid SQL Injection attacks using placeholders

--- Databases/contacts2.py
+++ Databases/contacts2.py
@@ -6,6 +6,11 @@ phone = input("Please provide the email to update the email: ")
 
 # update_sql = f"UPDATE contacts SET email = '{email}' WHERE phone = {phone}"
 update_sql = "UPDATE contacts SET email = ? WHERE phone = ?"  # Make changes to avoid SQL Injection attacks
+
+"""As seen in line 8 we have substituted the parameters used in string formatting with placeholders to avoid SQL
+Injection Attack. We all need to pass the placeholder values as tuple in the update_cursor.execute() method. All the 
+placeholder values should be packed as tuple enclosed in parenthesis. """
+
 print(update_sql)
 
 update_cursor = db.cursor()
