1f147712c38c7bc252b151aa388d49a53fce270f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-12 10:21:03 +0530

    Learn how to avoid SQL Injection attacks

--- Databases/contacts2.py
+++ Databases/contacts2.py
@@ -4,11 +4,12 @@ db = sqlite3.connect("contacts.sqlite")
 email = "newupdate@update.com"
 phone = input("Please provide the email to update the email: ")
 
-update_sql = f"UPDATE contacts SET email = '{email}' WHERE phone = {phone}"
+# update_sql = f"UPDATE contacts SET email = '{email}' WHERE phone = {phone}"
+update_sql = "UPDATE contacts SET email = ? WHERE phone = ?"  # Make changes to avoid SQL Injection attacks
 print(update_sql)
 
 update_cursor = db.cursor()
-update_cursor.executescript(update_sql)
+update_cursor.execute(update_sql, (email, phone))  # Add the tuple to avoid SQL Injection attacks
 print(f"{update_cursor.rowcount} rows updated")
 update_cursor.close()
 
