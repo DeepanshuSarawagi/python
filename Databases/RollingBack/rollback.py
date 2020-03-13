67ecfad229b4110b879b69f5ad1737b443c7f45f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-13 10:29:18 +0530

    Create new python module for learning transaction rollback in Databases

---
+++ Databases/RollingBack/rollback.py
@@ -0,0 +1,21 @@
+class Account(object):
+
+    def __init__(self, name: str, opening_balance: float = 0.0):
+        self.name = name
+        self._balance = opening_balance
+        print(f"Account created for {self.name}", end='')
+        self.show_balance()
+
+    def deposit(self, amount: float) -> float:
+        if amount > 0.0:
+            self._balance += amount
+            print(f"{amount} deposited.")
+        return self._balance
+
+    def withdraw(self, amount) -> float:
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            print(f"{amount} withdrawn.")
+            return amount
+        else:
+            print("Amount must be greater than zero and no more than balance.")
