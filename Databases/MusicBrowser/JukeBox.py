944d6f00f67fd7f6049aa9535b94ee0b542f507c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-15 15:54:58 +0530

    Configure columns in mainWindow using tkinter
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Databases/MusicBrowser/JukeBox.py
+++ Databases/MusicBrowser/JukeBox.py
@@ -6,4 +6,10 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Music Juke Box")
 mainWindow.geometry("1024x768")
 
+# configure columns
+mainWindow.columnconfigure(0, weight=2)
+mainWindow.columnconfigure(1, weight=2)
+mainWindow.columnconfigure(2, weight=2)
+mainWindow.columnconfigure(3, weight=1)  # Add spacer column in the end
+
 mainWindow.mainloop()
