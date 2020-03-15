d231bbeaa51073181778a43c0727cf8f53bce0ab
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-15 15:56:32 +0530

    Configure rows in mainWindow using tkinter
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Databases/MusicBrowser/JukeBox.py
+++ Databases/MusicBrowser/JukeBox.py
@@ -12,4 +12,10 @@ mainWindow.columnconfigure(1, weight=2)
 mainWindow.columnconfigure(2, weight=2)
 mainWindow.columnconfigure(3, weight=1)  # Add spacer column in the end
 
+# configure rows
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=5)
+mainWindow.rowconfigure(2, weight=5)
+mainWindow.rowconfigure(3, weight=1)
+
 mainWindow.mainloop()
