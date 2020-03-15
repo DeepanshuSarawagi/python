0a0178c87eb4bb20c339a85164837ce0f1e35b99
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-15 11:57:07 +0530

    Make changes in the checkDB.py to print both utc and local time of transaction
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Databases/RollingBack/checkDB.py
+++ Databases/RollingBack/checkDB.py
@@ -1,10 +1,12 @@
 import sqlite3
+import pytz
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
 for row in db.execute("SELECT * FROM history"):
     # print(row)
-    local_time = row[0]
-    print(f"{local_time} \t {type(local_time)}")
+    utc_time = row[0]
+    local_time = pytz.utc.localize(utc_time).astimezone()
+    print(f"{utc_time} \t {local_time}")
 
 db.close()
