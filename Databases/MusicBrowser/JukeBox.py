1d0ef970e4abc29cd66763d0ca4cef5d548045ed
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-15 16:11:02 +0530

    Create Artist, Albums and Songs list box
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Databases/MusicBrowser/JukeBox.py
+++ Databases/MusicBrowser/JukeBox.py
@@ -24,4 +24,23 @@ tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
 tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
+# Create Artists list box
+artistsList = tkinter.Listbox(mainWindow)
+artistsList.grid(row=1, column=0, rowspan=2, sticky='nsew', padx=(30, 0))
+artistsList.config(relief='sunken', border=2)
+
+# Create Albums list box
+albumLV = tkinter.Variable(mainWindow)
+albumLV.set(('Choose an artists',))
+albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
+albumList.grid(row=1, column=1, rowspan=2, sticky='nsew', padx=(30, 0))
+albumList.config(relief='sunken', border=2)
+
+# Songs list box
+songLV = tkinter.Variable(mainWindow)
+songLV.set(("Choose an album",))
+songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
+songList.config(relief='sunken', border=2)
+
 mainWindow.mainloop()
