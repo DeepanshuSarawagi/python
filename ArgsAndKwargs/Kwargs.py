7c2933fd729726b6332f02956c79af83dd0749f4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-16 10:39:27 +0530

    learn how to use **kwargs

---
+++ ArgsAndKwargs/Kwargs.py
@@ -0,0 +1,12 @@
+def print_backwards(*args, **kwargs):
+
+    """Just similar to *args which unpacks the tuple of arguments passed as parameter in functions, **kwargs will
+    unpack the dictionary of named keywords which is passed. In the below program, we will define a simple function
+    without specifying the named parameter file= yet pass it as an argument while calling the method"""
+
+    for word in args[::-1]:
+        print(word[::-1], end=' ', **kwargs)
+
+
+with open('backwards.txt', 'w') as backwards:
+    print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader", file=backwards)
