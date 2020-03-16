b7368b76d933706729cc77f0b68664c83368e5df
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-16 10:04:16 +0530

    Learn about position of *args in a function

--- ArgsAndKwargs/Args.py
+++ ArgsAndKwargs/Args.py
@@ -13,3 +13,19 @@ expect the unpacked tuple of arguments since we are passing 4 different argument
 very powerful feature when we have to pass multiple arguments to a function in python."""
 
 print(average(1, 2, 3, 4, 5, 6))
+
+
+def average_broken(first_arg, *args, second_arg):
+    print(type(args))
+    print(f"args is: {args}")  # this will print the arguments passed as tuple
+    print(f"*args is: ", *args)  # this will print the unpacked tuple of arguments
+    mean = 0
+    for arg in args:
+        mean += arg
+    return mean / len(args)
+
+
+print(average_broken(1, 2, 3, 4))
+
+"""this program will crash from line 18 since as per the documentation we have the specify *args at the end of list of all 
+the positional arguments. So it has to be def average(first_arg, second_arg, *args)"""
