f161fa99be32c290d7397c5ab57ac076f0171b0f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-17 10:55:54 +0530

    How to manipulate the key word arguments using kwargs

--- ArgsAndKwargs/Kwargs.py
+++ ArgsAndKwargs/Kwargs.py
@@ -1,15 +1,32 @@
+# def print_backwards(*args, **kwargs):
+#
+#     """Just similar to *args which unpacks the tuple of arguments passed as parameter in functions, **kwargs will
+#     unpack the dictionary of named keywords which is passed. In the below program, we will define a simple function
+#     without specifying the named parameter file= yet pass it as an argument while calling the method"""
+#     print(kwargs.keys())
+#     print(kwargs)
+#     kwargs.pop('end', None)
+#     print(kwargs)
+#     for word in args[::-1]:
+#         print(word[::-1], end=' ', **kwargs)
+
+
 def print_backwards(*args, **kwargs):
 
-    """Just similar to *args which unpacks the tuple of arguments passed as parameter in functions, **kwargs will
-    unpack the dictionary of named keywords which is passed. In the below program, we will define a simple function
-    without specifying the named parameter file= yet pass it as an argument while calling the method"""
     print(kwargs.keys())
     print(kwargs)
-    kwargs.pop('end', None)
+    end_character = kwargs.pop('end', '\n')
+    sep_character = kwargs.pop('sep', None)
     print(kwargs)
     for word in args[::-1]:
-        print(word[::-1], end=' ', **kwargs)
+        print(word[::-1], end=sep_character, **kwargs)
+    print(end=end_character)
 
 
 with open('backwards.txt', 'w') as backwards:
     print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='\n')
+    print('another string')
+
+print()
+print("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='\n', sep='|')
+print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='\n', sep='|')
