7396c8d4caa76dff9cd86c263761109e58b19306
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-19 11:37:44 +0530

    Link the artist list box and album list box using the requery method
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Databases/MusicBrowser/JukeBox.py
+++ Databases/MusicBrowser/JukeBox.py
@@ -58,12 +58,15 @@ def get_albums(event):
         index = lb.curselection()[0]
         artist_name = lb.get(index),
         # get the artist id from the database
-        artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
-        alist = []
-        for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", artist_id):
-            alist.append(row[0])
-        albumLV.set(tuple(alist))
-        songLV.set(("Choose an albums",))
+        artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()[0]
+        albumList.requery(artist_id)
+
+        # artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
+        # alist = []
+        # for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", artist_id):
+        #     alist.append(row[0])
+        # albumLV.set(tuple(alist))
+        # songLV.set(("Choose an albums",))
 
 
 def get_songs(event):
