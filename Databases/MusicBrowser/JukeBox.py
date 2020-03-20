4a58c0ae99125be05b751d229d338557cfbe12c5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-20 11:32:22 +0530

    Move the main program to prevent it from getting executed if the module is imported
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Databases/MusicBrowser/JukeBox.py
+++ Databases/MusicBrowser/JukeBox.py
@@ -1,8 +1,6 @@
 import sqlite3
 import tkinter
 
-conn = sqlite3.connect("music.sqlite")
-
 
 class Scrollbox(tkinter.Listbox):
 
@@ -73,70 +71,70 @@ class DataListBox(Scrollbox):
                 self.linked_box.requery(artist_id)
 
 
-mainWindow = tkinter.Tk()
-mainWindow.title("Music Juke Box")
-mainWindow.geometry("1024x768")
+if __name__ == "__main__":
+    conn = sqlite3.connect("music.sqlite")
+    mainWindow = tkinter.Tk()
+    mainWindow.title("Music Juke Box")
+    mainWindow.geometry("1024x768")
 
-# configure columns
-mainWindow.columnconfigure(0, weight=2)
-mainWindow.columnconfigure(1, weight=2)
-mainWindow.columnconfigure(2, weight=2)
-mainWindow.columnconfigure(3, weight=1)  # Add spacer column in the end
+    # configure columns
+    mainWindow.columnconfigure(0, weight=2)
+    mainWindow.columnconfigure(1, weight=2)
+    mainWindow.columnconfigure(2, weight=2)
+    mainWindow.columnconfigure(3, weight=1)  # Add spacer column in the end
 
-# configure rows
-mainWindow.rowconfigure(0, weight=1)
-mainWindow.rowconfigure(1, weight=5)
-mainWindow.rowconfigure(2, weight=5)
-mainWindow.rowconfigure(3, weight=1)
+    # configure rows
+    mainWindow.rowconfigure(0, weight=1)
+    mainWindow.rowconfigure(1, weight=5)
+    mainWindow.rowconfigure(2, weight=5)
+    mainWindow.rowconfigure(3, weight=1)
 
-# Create Labels
+    # Create Labels
 
-tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
-tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
-tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
+    tkinter.Label(mainWindow, text="Artists").grid(row=0, column=0)
+    tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
+    tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
-# Create Artists list box
-artistsList = DataListBox(mainWindow, conn, "artists", "name")
-artistsList.grid(row=1, column=0, rowspan=2, sticky='nsew', padx=(30, 0))
-artistsList.config(relief='sunken', border=2)
+    # Create Artists list box
+    artistsList = DataListBox(mainWindow, conn, "artists", "name")
+    artistsList.grid(row=1, column=0, rowspan=2, sticky='nsew', padx=(30, 0))
+    artistsList.config(relief='sunken', border=2)
 
-# insert the artist list data in the artist listbox from database
-# for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
-#     # print(artist)
-#     artistsList.insert(tkinter.END, artist[0])
+    # insert the artist list data in the artist listbox from database
+    # for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
+    #     # print(artist)
+    #     artistsList.insert(tkinter.END, artist[0])
 
-artistsList.requery()
+    artistsList.requery()
 
-# # artist scrollbar
-# artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistsList.yview)
-# artistScroll.grid(row=1, column=0, rowspan=2, sticky='nse')
-# artistsList['yscrollcommand'] = artistScroll.set
+    # # artist scrollbar
+    # artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistsList.yview)
+    # artistScroll.grid(row=1, column=0, rowspan=2, sticky='nse')
+    # artistsList['yscrollcommand'] = artistScroll.set
 
-# Create Albums list box
-albumLV = tkinter.Variable(mainWindow)
-albumLV.set(('Choose an artists',))
-albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))
-albumList.requery(12)
-albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
-albumList.config(relief='sunken', border=2)
+    # Create Albums list box
+    albumLV = tkinter.Variable(mainWindow)
+    albumLV.set(('Choose an artists',))
+    albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))
+    albumList.requery()
+    albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
+    albumList.config(relief='sunken', border=2)
 
-# albumList.bind('<<ListboxSelect>>', get_songs)
-artistsList.link(albumList, "artist")
+    # albumList.bind('<<ListboxSelect>>', get_songs)
+    artistsList.link(albumList, "artist")
 
-# albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
-# albumScroll.grid(row=1, column=1, sticky='nse')
-# albumList['yscrollcommand'] = albumScroll.set
+    # albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
+    # albumScroll.grid(row=1, column=1, sticky='nse')
+    # albumList['yscrollcommand'] = albumScroll.set
 
-# Songs list box
-songLV = tkinter.Variable(mainWindow)
-songLV.set(("Choose an album",))
-songList = DataListBox(mainWindow, conn, "songs", "title", sort_order=("track", "title"))
-songList.requery()
-songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
-songList.config(relief='sunken', border=2)
-albumList.link(songList, "album")
+    # Songs list box
+    songLV = tkinter.Variable(mainWindow)
+    songLV.set(("Choose an album",))
+    songList = DataListBox(mainWindow, conn, "songs", "title", sort_order=("track", "title"))
+    songList.requery()
+    songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
+    songList.config(relief='sunken', border=2)
+    albumList.link(songList, "album")
 
-testList = range(1, 100)
-albumLV.set(tuple(testList))
-mainWindow.mainloop()
-conn.close()
+    mainWindow.mainloop()
+    conn.close()
