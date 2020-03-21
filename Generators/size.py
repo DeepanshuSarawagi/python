46f9166eb2bcd09b7a1ef1b368fe8076f3e2bd4c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-21 10:39:23 +0530

    Use yield to check the size of object

--- Generators/size.py
+++ Generators/size.py
@@ -1,8 +1,19 @@
 import sys
 
-big_range = range(10000)
+
+def my_range(n: int):
+    start = 0
+    while start < n:
+        yield start
+        start += 1
+
+
+big_range = my_range(10000)
+# big_range = range(10000)
 
 print(f"The size of big range is {sys.getsizeof(big_range)} bytes")
+print(f" The size of my range is {sys.getsizeof(my_range)} bytes")
+
 
 big_list = []
 
