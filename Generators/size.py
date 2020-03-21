4da0a1d64c8ec75d750f7157b740ab7cde9974c5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-21 13:14:34 +0530

    To check how many times a generator is called

--- Generators/size.py
+++ Generators/size.py
@@ -2,14 +2,18 @@ import sys
 
 
 def my_range(n: int):
+    print("My range starts")
     start = 0
     while start < n:
+        print(f"my_range is returning: {start}")
         yield start
         start += 1
 
 
-big_range = my_range(10000)
+_ = input("line 12")
+big_range = my_range(5)
 # big_range = range(10000)
+_ = input("line 16")
 
 print(f"The size of big range is {sys.getsizeof(big_range)} bytes")
 print(f" The size of my range is {sys.getsizeof(my_range)} bytes")
@@ -22,3 +26,5 @@ for val in big_range:
 
 
 print(f"The size of big list is {sys.getsizeof(big_list)} bytes")
+print(big_list)
+print(big_range)
