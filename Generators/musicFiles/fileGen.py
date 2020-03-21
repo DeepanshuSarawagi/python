f38bccf3cc84305b4fba80f3aa8c87425dbcbab9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-21 18:03:34 +0530

    Use os.walk to walk through the file system

--- Generators/musicFiles/fileGen.py
+++ Generators/musicFiles/fileGen.py
@@ -2,6 +2,11 @@ import os
 
 root = "music"
 
+# for f in os.walk(root, topdown=True):
+#     print(f)
+# as we can learn from os.walk it prints tuples of three values which is
+# path, directory, files. We are going to unpack the tuple and print them
+
 for path, directories, files in os.walk(root, topdown=True):
     print(path)
     print(directories)
@@ -9,3 +14,4 @@ for path, directories, files in os.walk(root, topdown=True):
     input()
     # for f in files:
     #     print(f"\t{f}")
+
