ca52564d538df95a0392eb356be3e765815028f6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-22 15:20:14 +0530

    Under what are list comprehensions

---
+++ Comprehensions/ListComprehension/listComp.py
@@ -0,0 +1,12 @@
+print(__file__)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+squares = [number ** 2 for number in numbers]
+
+"""A list comprehension produces a list of items by evaluating the expression first for each 
+item in the list. Comprehensions are divided into two parts: 1) expression 2) Iteration
+number ** 2 is the expression and it returns the number squared. Iteration part 'for number in numbers'
+is iterated except for the fact that a semicolon is not required in the end."""
+
+print(squares)
