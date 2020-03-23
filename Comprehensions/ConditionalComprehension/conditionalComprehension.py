e53b9867190977b718b63b71c81f05986004a840
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-23 20:37:53 +0530

    Add docstrings for understanding conditional comprehension

--- Comprehensions/ConditionalComprehension/conditionalComprehension.py
+++ Comprehensions/ConditionalComprehension/conditionalComprehension.py
@@ -16,6 +16,13 @@ for meal in menu:
 
 print("Below is the solution using conditional comprehension")
 meals = [meal for meal in menu if "spam" not in meal]
+
+"""As we learned in previous lecture, list comprehension is a combination of expression and iteration. However, a 
+conditional comprehension contains 'expression, iteration and filters'. As you can see in the above code, 
+'if "spam" not in meal' is the filter which is applied to the conditional comprehension list. The list appends the items
+only when the filter satisfies the condition.
+"""
+
 print(meals)
 for good_meal in meals:  # iterate over the list of items when retrieved using conditional comprehension
     print(good_meal)
