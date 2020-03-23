fa7f0f552a403b12722937bac589e52b35b52aec
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-23 20:57:15 +0530

    Learn about complex conditional list comprehension

--- Comprehensions/ConditionalComprehension/conditionalComprehension.py
+++ Comprehensions/ConditionalComprehension/conditionalComprehension.py
@@ -1,6 +1,6 @@
 menu = [
     ["eggs", "spam", "eggs"],
-    ["eggs, ""bread", "bacon"],
+    ["eggs", "bread", "bacon"],
     ["bread", "butter", "jam"],
     ["panini", "tofu", "cheese"],
     ["eggs", "spam", "spam", "milk"],
@@ -26,3 +26,19 @@ only when the filter satisfies the condition.
 print(meals)
 for good_meal in meals:  # iterate over the list of items when retrieved using conditional comprehension
     print(good_meal)
+
+# Complex conditional list comprehension
+
+"""Let's say we want a menu which doesn' contain 'spam' as well as 'eggs'. We can create conditional filters using
+and keyword"""
+
+veg_meals = [meal for meal in menu if "spam" not in meal and "eggs" not in meal]  # this will get us the list which
+# doesnt have spam and eggs
+
+print(veg_meals)
+
+"""Lets say there is a customer who doesn't like 'eggs', 'spam', and 'bread and butter' together. Below is the code"""
+
+diet_meals = [meal for meal in menu if "spam" not in meal and "eggs" not in meal if not ("bread" in meal and "butter" in meal)]
+print(f"Diet meal menu is {diet_meals}")
+
