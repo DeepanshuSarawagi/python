0e0150556f757fb202f86c0c432853bb1a7ccca1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 16:00:52 +0530

    Calculate mean and standard deviation
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Map/reduceIntro.py
+++ Map/reduceIntro.py
@@ -1,5 +1,6 @@
 import timeit
 import functools
+from statistics import mean, stdev
 
 
 def calc_values(x, y: int):
@@ -12,3 +13,6 @@ print(reduced_values)
 
 if __name__ == "__main__":
     print(timeit.timeit("reduced_values", globals=globals(), number=100000))
+    list1 = timeit.repeat("reduced_values", globals=globals(), number=100000)
+    print(list1)
+    print(f"Mean value of reduce function is {mean(list1)} and standard deviation is {stdev(list1)}")
