08033f88d0b79a05905cd04c6a1b7ccb367c9197
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 12:30:12 +0530

    recreate project

--- Map/mapIntro.py
+++
@@ -1,8 +0,0 @@
-string = "What have the romans ever done for us"
-
-capitals = [word.upper() for word in string]
-print(capitals)
-
-words = [word.upper() for word in string.split(' ')]
-print(words)
-
