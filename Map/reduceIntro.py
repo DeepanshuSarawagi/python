b3699f45ad15b5b576d3848168f46e7118373cdb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 15:57:19 +0530

    Use timeit module to calculate the performance of reduce method
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Map/reduceIntro.py
+++ Map/reduceIntro.py
@@ -9,3 +9,6 @@ def calc_values(x, y: int):
 numbers = [2, 3, 5, 8, 13]
 reduced_values = functools.reduce(calc_values, numbers)
 print(reduced_values)
+
+if __name__ == "__main__":
+    print(timeit.timeit("reduced_values", globals=globals(), number=100000))
