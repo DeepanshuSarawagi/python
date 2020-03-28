08677dbe7bfc1477066a454bb5e855928213fa31
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 12:35:57 +0530

    Use map function to print the strings in upper case
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Map/mapIntro.py
+++ Map/mapIntro.py
@@ -3,5 +3,16 @@ string = "What have the Romans ever done for us"
 capitals = [word.upper() for word in string]
 print(capitals)
 
+# Use map to get the same results.
+map_capitals = list(map(str.upper, string))
+print(map_capitals)
+print()
+
+
 words = [word.upper() for word in string.split(' ')]
 print(words)
+
+# Use map to print the capital words
+map_words = list(map(str.upper, string.split(' ')))
+print(map_words)
+
