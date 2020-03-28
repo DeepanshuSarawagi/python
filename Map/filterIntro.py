ca21950a0f9159f0ac9b0769e052222d655a5fe5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 15:27:03 +0530

    use the timeit module to compare the results
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Map/filterIntro.py
+++ Map/filterIntro.py
@@ -1,3 +1,5 @@
+import timeit
+
 menu = [["eggs", "spam", "milk"],
         ["milk", "bread", "butter"],
         ["bread", "bacon", "cheese"],
@@ -14,8 +16,12 @@ for meal in menu:
 print("------------------------------")
 # using list comprehension
 
-meals = [meal for meal in menu if "spam" not in meal]
-print(meals)
+
+def spamless_comp():
+    # meals = [meal for meal in menu if "spam" not in meal]
+    meals = [meal for meal in menu if not_spam(meal)]
+    return meals
+
 
 print("------------------------------")
 
@@ -24,6 +30,11 @@ def not_spam(meal_list: list):
     return "spam" not in meal_list
 
 
-spamless_meal = list(filter(not_spam, menu))
-print(spamless_meal)
+def spamless_filter():
+    spamless_meal = list(filter(not_spam, menu))
+    return spamless_meal
 
+
+if __name__ == "__main__":
+    print(timeit.timeit(spamless_comp, number=100000))
+    print(timeit.timeit(spamless_filter, number=100000))
