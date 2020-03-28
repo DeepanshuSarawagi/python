d981611736e1d1594b96dcff77e97859bc095719
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-03-28 12:57:36 +0530

    Solve the Udemy challenge
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- Map/mapIntro.py
+++ Map/mapIntro.py
@@ -1,20 +1,49 @@
+import timeit
+
 string = "What have the Romans ever done for us"
 
-capitals = [word.upper() for word in string]
-print(capitals)
+
+def caps():
+    capitals = [word.upper() for word in string]
+    return capitals
+
 
 # Use map to get the same results.
-map_capitals = list(map(str.upper, string))
-print(map_capitals)
+
+def map_caps():
+    map_capitals = list(map(str.upper, string))
+    return map_capitals
+
+
 print()
 
 
-words = [word.upper() for word in string.split(' ')]
-print(words)
+def words_caps():
+    words = [word.upper() for word in string.split(' ')]
+    return words
+
 
 # Use map to print the capital words
-map_words = list(map(str.upper, string.split(' ')))
-print(map_words)
+def words_map():
+    map_words = list(map(str.upper, string.split(' ')))
+    return map_words
 
-for x in map(str.upper, string.split(' ')):
-    print(x)
+
+def map_iter():
+    for x in map(str.upper, string.split(' ')):
+        return x
+
+
+result1 = timeit.timeit(caps, globals=globals(), number=10000)
+result2 = timeit.timeit(map_caps, globals=globals(), number=10000)
+result3 = timeit.timeit(words_caps, globals=globals(), number=10000)
+result4 = timeit.timeit(words_map, globals=globals(), number=10000)
+result5 = timeit.timeit(map_iter, globals=globals(), number=10000)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+
+
