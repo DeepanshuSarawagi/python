9c7231594a61cd2da1a37059b7036ee13bca4a78
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-03 12:44:55 +0530

    Complete exercise 2 on freeCodeCamp

--- freeCodeCamp/USFloorApp/USFloor.py
+++ freeCodeCamp/USFloorApp/USFloor.py
@@ -1,5 +1,6 @@
 # This is a US Floor app exercise on freeCodeCamp
 
+
 EUFloor = int(input("Enter on which floor you are? "))
 print("The European floor is {0}".format(EUFloor))
 USFloor = EUFloor + 1
