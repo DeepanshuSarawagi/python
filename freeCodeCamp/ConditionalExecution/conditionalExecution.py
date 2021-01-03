1282c791af6eae2d9afb81a4419b08dc1f8af5dc
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-03 13:11:03 +0530

    Complete exercise 4 of FCC's python course

---
+++ freeCodeCamp/ConditionalExecution/conditionalExecution.py
@@ -0,0 +1,14 @@
+# This is a python exercise on freeCodeCamp's python certification curriculum
+
+x = 5
+if x < 5:
+    print("X is less than 5")
+
+for i in range(5):
+    print(i)
+    if i <= 2:
+        print("i is less than or equal to 2")
+    if i > 2:
+        print("i is now ", i)
+    print("Done with ", i)
+print("All done!")
