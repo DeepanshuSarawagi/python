9ea6c0805d826284472062994b111844dae6b84d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-03 12:00:39 +0530

    A simple USFloorApp using Python

---
+++ freeCodeCamp/USFloorApp/USFloor.py
@@ -0,0 +1,8 @@
+# This is a US Floor app exercise on freeCodeCamp
+
+EUFloor = int(input("Enter on which floor you are? "))
+print("The European floor is {0}".format(EUFloor))
+USFloor = EUFloor + 1
+print("You are on US floor: {0}".format(USFloor))
+
+
