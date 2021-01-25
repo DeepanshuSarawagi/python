963b6bb948f93b80ad8aa1388eb4dcdaced39057
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-25 21:50:39 +0530

    Using input() inside print()

---
+++ 100DaysOfPython/Day1/inputFunction.py
@@ -0,0 +1,10 @@
+"""
+We are going to use the input function inside the print function
+"""
+name = input("What is your name? ")
+if name == "deepanshu":
+    print("Hello " + name)
+else:
+    print("Enter your valid first name")
+
+print("Hello Mr. " + input("What is your last name? "))
