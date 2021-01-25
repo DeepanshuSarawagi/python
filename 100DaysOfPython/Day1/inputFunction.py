09e921c6d87016b9b18af36891b71de296f925ee
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-25 22:06:30 +0530

    Complete coding exercise 3 on Day 1

--- 100DaysOfPython/Day1/inputFunction.py
+++ 100DaysOfPython/Day1/inputFunction.py
@@ -8,3 +8,8 @@ else:
     print("Enter your valid first name")
 
 print("Hello Mr. " + input("What is your last name? "))
+
+# Day 1 Exercise 3 - Print the number of characters in a String
+
+name = input("Please enter your username: ")
+print(len(name))
