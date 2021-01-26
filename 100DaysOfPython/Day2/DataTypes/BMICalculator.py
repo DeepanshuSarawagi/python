1c91177e63a9db9c7cfbc117dd7abacd8a3a2e2b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-26 20:31:55 +0530

    Difference between floating and integer division

--- 100DaysOfPython/Day2/DataTypes/BMICalculator.py
+++ 100DaysOfPython/Day2/DataTypes/BMICalculator.py
@@ -1,3 +1,5 @@
 height = float(input("Enter your height in meters: "))
 weight = float(input("Enter your weight in kilograms: "))
 print("Your BMI is {}".format(round(weight / (height * height), 2)))
+print(8 // 3)
+print(8 / 3)
