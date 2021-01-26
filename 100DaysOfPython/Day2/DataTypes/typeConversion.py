3435216dd6f828a60007f3a576b113d0cedc81e9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-26 19:46:19 +0530

    Solve Day 2 - Exercise 1

--- 100DaysOfPython/Day2/DataTypes/typeConversion.py
+++ 100DaysOfPython/Day2/DataTypes/typeConversion.py
@@ -7,3 +7,14 @@ print("Your name has " + str(num_char) + " characters")  # Type conversion happe
 print("Your name has {} characters".format(num_char))
 
 print(70 + float("170.5"))
+
+# Day 2 - Exercise 1 - Print the sum of digits of a number
+two_digit_number = input("Type a two digit number of your choice: ")
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+# Better solution
+
+sum_of_numbers = 0
+for i in range(0, len(str(two_digit_number))):
+    sum_of_numbers += int(two_digit_number[i])
+print(sum_of_numbers)
