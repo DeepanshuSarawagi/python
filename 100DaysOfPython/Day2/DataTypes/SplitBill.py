b7e4480e73ccf6e43a3e7c3a83914d8ab64b8ad0
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-26 22:13:39 +0530

    Day 2 - Simple Project - Bill and tip calculator

---
+++ 100DaysOfPython/Day2/DataTypes/SplitBill.py
@@ -0,0 +1,6 @@
+bill_amount = float(input("What was the total bill?\n"))
+tip = int(input("What is the percentage of tip you would like to give? 12, 14 or 15?\n"))
+tip_on_bill = float((bill_amount * tip) / 100)
+total_bill = float(bill_amount + tip_on_bill)
+no_of_persons = int(input("How many people to split the bill?\n"))
+print("Each person should pay {}".format(round(total_bill / no_of_persons, 2)))
