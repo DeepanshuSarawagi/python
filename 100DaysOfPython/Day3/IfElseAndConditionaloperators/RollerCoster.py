b15945b978028fbb106a68abfb776c3941f080bb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-27 19:34:17 +0530

    Day 3 - If Else statements

---
+++ 100DaysOfPython/Day3/IfElseAndConditionaloperators/RollerCoster.py
@@ -0,0 +1,8 @@
+# We are going to use If-Else statement here
+
+print("Welcome to the Jumbo Roller Coaster")
+height = int(input("What is your height in cm?\n"))
+if height >= 120:
+    print("Welcome aboard!")
+else:
+    print("Sorry! You cannot take the ride due to safety concerns!")
