d865387ad149b48e5be2a34f6fd409fab99cb71a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-27 19:39:45 +0530

    Day 3 - Exercise 1 - Odd or even

--- 100DaysOfPython/Day3/IfElseAndConditionaloperators/RollerCoster.py
+++ 100DaysOfPython/Day3/IfElseAndConditionaloperators/RollerCoster.py
@@ -6,3 +6,11 @@ if height >= 120:
     print("Welcome aboard!")
 else:
     print("Sorry! You cannot take the ride due to safety concerns!")
+
+# Day 3 Exercise 1 - To find out if a number is odd or even
+
+num = int(input("Enter a number of your choice: "))
+if num % 2 == 0:
+    print("{} is an even number".format(num))
+else:
+    print("{} is an odd number".format(num))
