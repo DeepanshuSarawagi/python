85e869b08abbaa717947926a08e9ac5bcca4192f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-27 20:05:56 +0530

    Day 3 - BMI Calculator with BMI remarks using nested elif statements

---
+++ 100DaysOfPython/Day3/IfElseAndConditionaloperators/BMICalculator.py
@@ -0,0 +1,15 @@
+# BMI calculator 2.0
+height = float(input("Please enter your height in meters: "))
+weight = float(input("Please enter your weight in kgs: "))
+
+bmi = float(round(weight / (height ** 2), 2))
+if bmi <= 18.5:
+    print("BMI = {:.2f}. You are underweight".format(bmi))
+elif 18.5 < bmi <= 25:
+    print("BMI = {:.2f}. You are normal weight.".format(bmi))
+elif 25 < bmi <= 30:
+    print("BMI = {:.2f}. You are overweight.".format(bmi))
+elif 30 < bmi <= 35:
+    print("BMI = {:.2f}. You are obese.".format(bmi))
+else:
+    print("BMI = {:.2f}. You are clinically obese.".format(bmi))
