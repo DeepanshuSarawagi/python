98274333322de51d084fa04199fc20d93f60dfd4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-30 22:21:12 +0530

    Basic use of for loop

--- 100DaysOfPython/Day5/Loops/forLoop.py
+++ 100DaysOfPython/Day5/Loops/forLoop.py
@@ -15,3 +15,21 @@ for student in student_heights:
     sum_of_height += int(student)
 
 print("Average students height = {}".format(round(sum_of_height / count)))
+
+# Day 5 - Exercise 2 - Print the highest score from the list
+
+high_scores = input("Enter the high scores of the students separated by commas and space: ")
+high_scores = high_scores.split(", ")
+
+highest_score = 0
+
+for high_score in high_scores:
+    if int(high_score) > highest_score:
+        highest_score = int(high_score)
+
+print(highest_score)
+lowest = 100
+for i in range(0, (len(high_scores) - 1)):
+    if int(high_scores[i]) < lowest:
+        lowest = int(high_scores[i])
+print(lowest)
