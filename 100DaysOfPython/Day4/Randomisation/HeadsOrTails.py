8b5587a3d207da5b2b5759b03da9ebeca47a5c0c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-30 14:50:48 +0530

    Coin tossing program

--- 100DaysOfPython/Day4/Randomisation/HeadsOrTails.py
+++ 100DaysOfPython/Day4/Randomisation/HeadsOrTails.py
@@ -3,7 +3,7 @@ import random
 print("Welcome to the virtual coin toss program")
 for i in range(0, 10):
     toss = random.randint(0, 1)
-    if toss == 0:
+    if toss == 1:
         print("You got Heads this time")
     else:
         print("You got Tails this time")
