172613f076584853a493b4e6b9b5bde08553929d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-30 21:05:03 +0530

    Complete the simple for loop challenge

--- 100DaysOfPython/Day5/Loops/forLoop.py
+++ 100DaysOfPython/Day5/Loops/forLoop.py
@@ -4,3 +4,14 @@ fruits = ["Apple", "Peach", "Pear"]
 
 for fruit in fruits:
     print(fruit)
+
+student_heights = input("Please enter the student heights separated by comma and space: ")
+student_heights = student_heights.split(", ")
+
+count = 0
+sum_of_height = 0
+for student in student_heights:
+    count += 1
+    sum_of_height += int(student)
+
+print("Average students height = {}".format(round(sum_of_height / count)))
