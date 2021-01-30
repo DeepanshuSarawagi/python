7372a1f60f0220a18793a6896e0e7b7079707ce6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-30 15:54:13 +0530

    Simplified solution using the random.choice() method

--- 100DaysOfPython/Day4/Lists/PayTheBillChallenge.py
+++ 100DaysOfPython/Day4/Lists/PayTheBillChallenge.py
@@ -3,3 +3,7 @@ names = input("Please enter the names of people who joined for dinner separated 
 list_of_names = names.split(", ")
 randomChoice = random.randint(0, (len(list_of_names) - 1))
 print("{} will pay the bill today!".format(list_of_names[randomChoice]))
+
+# Simplified solution by instructor using the random.choice() method to randomly pick an item from an iterative object
+person_to_pay = random.choice(list_of_names)
+print(person_to_pay + " will pay the bill today")
