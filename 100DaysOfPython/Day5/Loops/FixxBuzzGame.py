8f74ad94a80d188d370f9ac1de64af463e7b7da5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-31 13:08:52 +0530

    Complete the fizzBuzz challenge

---
+++ 100DaysOfPython/Day5/Loops/FixxBuzzGame.py
@@ -0,0 +1,11 @@
+# This is a challenge exercise - We will write the program to play the fizzBuzz game
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
