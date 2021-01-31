c461687e532fffc5491d8117c0aae9fba5208402
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-01-31 12:11:47 +0530

    Sum of first hundred even numbers

--- 100DaysOfPython/Day5/Loops/forLoopsRange.py
+++ 100DaysOfPython/Day5/Loops/forLoopsRange.py
@@ -4,3 +4,10 @@ sum_of_numbers = 0
 for i in range(1, 101):
     sum_of_numbers += i
 print(sum_of_numbers)
+
+# Day 5 - Exercise 3
+
+sum_of_even = 0
+for i in range(0, 101, 2):
+    sum_of_even += i
+print(sum_of_even)
