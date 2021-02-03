7a60eaddf64a80eb601734bff13e66dcaddf3e22
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-03 17:27:50 +0530

    Day 8 - Review basics of functions

---
+++ 100DaysOfPython/Day8/FunctionParameters/Greetings.py
@@ -0,0 +1,9 @@
+# In this lesson we are going to review the basics of Functions
+
+def greet():
+    print("Hello!")
+    print("Welcome to the coders club!")
+    print("Have a great day!")
+
+
+greet()
