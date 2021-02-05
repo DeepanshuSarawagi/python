0677a43fa16fd552ae381d29f82889adc48e7908
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-05 17:03:27 +0530

    Adding new items in the dictionary

--- 100DaysOfPython/Day9/Dictionaries/DictionaryBasics.py
+++ 100DaysOfPython/Day9/Dictionaries/DictionaryBasics.py
@@ -1,7 +1,13 @@
 # In this lesson we are going to learn about the dictionaries in Python
 
-programming_dictionary = {"bug": "A piece of code which prevents the program from running as expected",
-                          "function": "A program which can be called again and again"}
+programming_dictionary = {
+                        "bug": "A piece of code which prevents the program from running as expected",
+                        "function": "A program which can be called again and again"
+                        }
+
+# Adding new items to the dictionary
+
+programming_dictionary["Loop"] = "The action of doing something again and again."
 
 for keys in programming_dictionary.keys():
     print("{}: {}".format(keys, programming_dictionary[keys]))
