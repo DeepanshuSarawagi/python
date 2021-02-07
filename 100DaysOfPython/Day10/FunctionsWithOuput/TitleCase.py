7520e1489bb916daade7fb7c321dd76f4e4ad85b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-07 13:03:06 +0530

    Complete Day 10 - Exercise 2

--- 100DaysOfPython/Day10/FunctionsWithOuput/TitleCase.py
+++ 100DaysOfPython/Day10/FunctionsWithOuput/TitleCase.py
@@ -3,7 +3,7 @@
 
 def format_name(f_name, l_name):
     if f_name == "" or l_name == "":
-        return                     # This is going to end the function since we are asking the function to quit
+        return  # This is going to end the function since we are asking the function to quit
     f_name_titled = title_case(f_name)
     l_named_titled = title_case(l_name)
     return f_name_titled + " " + l_named_titled
@@ -24,3 +24,29 @@ def title_case(name):
 
 final_name = format_name(f_name="deEPanShU", l_name="sARaWaGI")
 print(final_name)
+
+
+# Day 10 Exercise 1
+
+def is_leap_year(year):
+    if year % 4 == 0 or year % 400 == 0:
+        if year % 100 == 0 and year % 400 != 0:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap_year(year) == True and month == 2:
+        return month_days[1] + 1
+    else:
+        return month_days[month - 1]
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month between 1 and 12: "))
+days = days_in_month(year=year, month=month)
+print(days)
