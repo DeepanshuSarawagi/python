52ffc11e22d1affa988b796fc6f50fd2e17be841
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-07 13:22:39 +0530

    Define methods for creating CalculatorProgram

---
+++ 100DaysOfPython/Day10/FunctionsWithOuput/CalculatorProgram.py
@@ -0,0 +1,28 @@
+"""
+In this project we are going to create a simple Calculator program.
+This program will do basic mathematical operations like add, subtract, multiply and divide
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": "Addition",
+    "-": "Subtraction",
+    "*": "Multiplication",
+    "/": "Division"
+    }
