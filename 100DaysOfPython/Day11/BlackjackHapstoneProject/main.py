c0d6fe2a365d5c859a215e4a1f1f4aaa29493570
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-11 21:12:16 +0530

    Define calculate_score() fn

--- 100DaysOfPython/Day11/BlackjackHapstoneProject/main.py
+++ 100DaysOfPython/Day11/BlackjackHapstoneProject/main.py
@@ -3,6 +3,8 @@ import random
 
 
 def deal_card():
+    """Returns a random card from the deck."""
+
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
     card = random.choice(cards)
     return card
@@ -10,3 +12,13 @@ def deal_card():
 
 user_cards = []
 computer_cards = []
+
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+def calculate_score(cards):
+    return sum(cards)
+
