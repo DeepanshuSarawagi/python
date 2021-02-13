43439207f97daa8ea1bd34b8b01d0b94daef2616
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-13 23:09:32 +0530

    Create initial guessing game number

--- 100DaysOfPython/Day12/Scope/GuessingGame.py
+++ 100DaysOfPython/Day12/Scope/GuessingGame.py
@@ -1,1 +1,15 @@
+# In this mini project we are going to create a number guessing game
+import random
+
+print("I'm thinking of a number between 1 and 100.")
+num = random.randint(1, 100)
+
+difficulty = input("Choose the difficult to guess the number. Type 'easy' or 'hard': ").lower()
+
+guess = int(input("Make a guess: "))
+
+def number_guessing():
+    count = 0
+    if difficulty == "easy":
+        print("You have 10 attempts to guess the number: ")
 
