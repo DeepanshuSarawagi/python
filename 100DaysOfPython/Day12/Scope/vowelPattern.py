abb64374c0841449d9a7b4f594d790f19e66d73c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-23 20:32:02 +0530

    Fix the while loop

--- 100DaysOfPython/Day12/Scope/vowelPattern.py
+++ 100DaysOfPython/Day12/Scope/vowelPattern.py
@@ -11,22 +11,27 @@ s = ""
 
 s += letter
 
-count = 1
+length = len(s)
+print(length)
+count = 0
 
-while count < 6:
-    new_index = index_of_vowel + count
-    next_char = ""
-    if new_index > 25:
-        next_char = alphabets[-25-1]
-        print(next_char)
-    else:
-        next_char = alphabets[new_index]
-        print(next_char)
-    print(next_char in vowels)
-    if next_char in vowels:
-        print("Found a vowel")
-        continue
-    s += next_char
-    count += 1
+if letter in vowels:
+    while length < 7:
+        count += 1
+        new_index = index_of_vowel + count
+        print(new_index)
+        if new_index > 25:
+            next_char = alphabets[-25-1]
+            print("Next index is greater than 25. {}".format(next_char))
+        else:
+            next_char = alphabets[new_index]
+            print("Next index is lesser than 25. {}".format(next_char))
+        if next_char not in vowels:
+            s += next_char
+            length = len(s)
+        else:
+            continue
+else:
+    print("It is not a vowel. Please try again.")
 
 print(s)
