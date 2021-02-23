1a8fba229d5c9171bd3b4c863fb1b8249bf3b260
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-23 21:17:29 +0530

    Fix the while loop

--- 100DaysOfPython/Day12/Scope/vowelPattern.py
+++ 100DaysOfPython/Day12/Scope/vowelPattern.py
@@ -11,26 +11,49 @@ s = ""
 
 s += letter
 
-length = len(s)
-print(length)
 count = 0
+length = len(s)
+
+
+def append_char(char):
+    global s
+    s += char
+    return s
+
 
 if letter in vowels:
-    while length < 7:
+    while not length >= 6:
         count += 1
         new_index = index_of_vowel + count
-        print(new_index)
+        print("new index is now: {}".format(new_index))
         if new_index > 25:
-            next_char = alphabets[-25-1]
+            next_char = alphabets[(new_index - 26)]
             print("Next index is greater than 25. {}".format(next_char))
+            if next_char not in vowels and length <= 6:
+                s = append_char(next_char)
+                print(s)
+                length = len(s)
+                print("length is now: {}".format(length))
+            else:
+                length = len(s)
+                continue
         else:
             next_char = alphabets[new_index]
             print("Next index is lesser than 25. {}".format(next_char))
-        if next_char not in vowels:
-            s += next_char
-            length = len(s)
-        else:
-            continue
+            if next_char not in vowels and length <= 6:
+                s = append_char(next_char)
+                print(s)
+                length = len(s)
+                print("length is now: {}".format(length))
+            else:
+                length = len(s)
+                continue
+        # if next_char not in vowels:
+        #     s += next_char
+        #     length = len(s)
+        #     print("length is now: {}".format(length))
+        # else:
+        #     continue
 else:
     print("It is not a vowel. Please try again.")
 
