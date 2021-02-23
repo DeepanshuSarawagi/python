fff27dc6734ac46ed7f539e8b516acfd358dc76e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-23 21:19:57 +0530

    Remove unnecessary prints

--- 100DaysOfPython/Day12/Scope/vowelPattern.py
+++ 100DaysOfPython/Day12/Scope/vowelPattern.py
@@ -25,35 +25,22 @@ if letter in vowels:
     while not length >= 6:
         count += 1
         new_index = index_of_vowel + count
-        print("new index is now: {}".format(new_index))
         if new_index > 25:
             next_char = alphabets[(new_index - 26)]
-            print("Next index is greater than 25. {}".format(next_char))
             if next_char not in vowels and length <= 6:
                 s = append_char(next_char)
-                print(s)
                 length = len(s)
-                print("length is now: {}".format(length))
             else:
                 length = len(s)
                 continue
         else:
             next_char = alphabets[new_index]
-            print("Next index is lesser than 25. {}".format(next_char))
             if next_char not in vowels and length <= 6:
                 s = append_char(next_char)
-                print(s)
                 length = len(s)
-                print("length is now: {}".format(length))
             else:
                 length = len(s)
                 continue
-        # if next_char not in vowels:
-        #     s += next_char
-        #     length = len(s)
-        #     print("length is now: {}".format(length))
-        # else:
-        #     continue
 else:
     print("It is not a vowel. Please try again.")
 
