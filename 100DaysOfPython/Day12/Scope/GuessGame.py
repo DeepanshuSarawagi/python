e7b65b50012b36acd7d91b351937f99d13047adf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-24 21:06:05 +0530

    Create a function to set the difficulty

--- 100DaysOfPython/Day12/Scope/GuessGame.py
+++ 100DaysOfPython/Day12/Scope/GuessGame.py
@@ -1,5 +1,8 @@
 import random
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 
 def check_answer(guess, answer):
     if guess > answer:
@@ -8,6 +11,14 @@ def check_answer(guess, answer):
         print("Too low.")
     else:
         print("You got it right. The answer was {}".format(answer))
+
+
+def set_difficulty():
+    level = input("Choose a difficult 'easy' or 'hard': ").lower()
+    if level == "easy":
+        turns = EASY_LEVEL_TURNS
+    else:
+        turns = HARD_LEVEL_TURNS
 
 
 print("Welcome to the guessing game!")
