3f51809b5e94eac734b93d933fd60a7d45307fc4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-26 21:04:08 +0530

    Get the game logic complete

--- 100DaysOfPython/Day14/HigherLowerGameProject/main.py
+++ 100DaysOfPython/Day14/HigherLowerGameProject/main.py
@@ -10,30 +10,49 @@ import random
 #         print("{}: {}".format(list_of_keys[j], list_value[j]))
 
 
-for i in game_data.data:
-    for key, value in i.items():
-        print(key, value)
-    print()
-
 first_random = random.choice(game_data.data)
 first_item = list(first_random.values())
 second_random = random.choice(game_data.data)
 second_item = list(second_random.values())
+if first_item[0] == second_item[0]:
+    second_random = random.choice(game_data.data)
+    second_item = list(second_random.values())
 
-print(first_random)
-print(second_random)
 
-print()
+score = 0
+got_it_right = True
 
-print("Compare {}: {}, {}, {}, {}".format("A", first_item[0], first_item[1], first_item[2], first_item[3]))
-print("Compare {}: {}, {}, {}, {}".format("B", second_item[0], second_item[1], second_item[2], second_item[3]))
 
-choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+def play_game():
+    global first_item
+    global second_item
+    global score
+    global got_it_right
+    print("Compare {}: {}, {}, {}, {}".format("A", first_item[0], first_item[1], first_item[2], first_item[3]))
+    print("Compare {}: {}, {}, {}, {}".format("B", second_item[0], second_item[1], second_item[2], second_item[3]))
+    choice = input("Who has more followers? Type 'A' or 'B': ").upper()
 
-if choice == "A":
-    if first_item[1] > second_item[1]:
-        first_item = second_item
+    if choice == "A":
+        if first_item[1] > second_item[1]:
+            score += 1
+            got_it_right = True
+            print("You got it right. Current score: {}".format(score))
+        else:
+            print("Sorry you got it wrong! Final score: {}".format(score))
+            got_it_right = False
+    elif choice == "B":
+        if second_item[1] > first_item[1]:
+            score += 1
+            got_it_right = True
+            print("You got it right. Current score: {}".format(score))
+        else:
+            print("Sorry you got it wrong! Final score: {}".format(score))
+            got_it_right = False
+    first_item = second_item
+    second_item = list(random.choice(game_data.data).values())
+    if second_item == first_item:
         second_item = list(random.choice(game_data.data).values())
-        print(first_item)
-        print(second_item)
 
+
+while got_it_right:
+    play_game()
