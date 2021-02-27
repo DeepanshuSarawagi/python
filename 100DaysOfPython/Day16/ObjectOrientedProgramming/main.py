c314177ebb58d5dbafd835d27aebf4f0afdd63f4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-27 20:25:29 +0530

    Learn about color() method in turtle GUI

--- 100DaysOfPython/Day16/ObjectOrientedProgramming/main.py
+++ 100DaysOfPython/Day16/ObjectOrientedProgramming/main.py
@@ -2,3 +2,10 @@ import turtle
 
 timmy = turtle.Turtle()  # Constructed a New turtle object of class Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+my_screen.exitonclick()
+
