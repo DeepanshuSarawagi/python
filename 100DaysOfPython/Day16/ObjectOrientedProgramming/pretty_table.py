bb3c481f0265d9d768d117251a85043391e5ccc2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-27 21:27:14 +0530

    Learn how to align a table and print only specific rows

--- 100DaysOfPython/Day16/ObjectOrientedProgramming/pretty_table.py
+++ 100DaysOfPython/Day16/ObjectOrientedProgramming/pretty_table.py
@@ -2,4 +2,7 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
 print(table)
+print()
+print(table.get_string(start=0, end=2))  # This will print the rows starting from 1 till 2
