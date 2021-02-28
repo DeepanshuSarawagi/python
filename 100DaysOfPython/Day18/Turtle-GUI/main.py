13c49e61a743d7230b8e6fe4b403c943d3f4e66a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-28 15:53:17 +0530

    Move the turtle

--- 100DaysOfPython/Day18/Turtle-GUI/main.py
+++ 100DaysOfPython/Day18/Turtle-GUI/main.py
@@ -2,5 +2,21 @@ from turtle import Turtle
 from turtle import Screen
 
 timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("tomato")
+timmy.forward(100)
+timmy.back(100)
+
+
+
+
+
+
+
+
+
+
+
+
 screen = Screen()
 screen.exitonclick()
