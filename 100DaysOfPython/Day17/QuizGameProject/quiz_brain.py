a9db6c49beae8566ab90a5e66f10eb232cfa357e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-28 15:06:16 +0530

    Create new check_answer() method

--- 100DaysOfPython/Day17/QuizGameProject/quiz_brain.py
+++ 100DaysOfPython/Day17/QuizGameProject/quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def next_question(self):
         question = self.question_list[self.question_number]
@@ -14,10 +15,20 @@ class QuizBrain:
                 ans += choice[i].upper()
             else:
                 ans += choice[i]
-        print(ans)
+        self.check_answer(ans, question.answer)
 
     def still_has_question(self):
+
         if self.question_number < len(self.question_list):
             return True
         else:
             return False
+
+    def check_answer(self, answer, correct_answer):
+
+        if answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!!")
+        else:
+            print("That's wrong!!")
+        print("The correct answer was {}.".format(correct_answer))
