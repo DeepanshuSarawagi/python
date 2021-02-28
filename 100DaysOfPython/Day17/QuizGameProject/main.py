36f69a01bfa4ca376a70e9802bba70cb1eccf49c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-02-28 15:15:15 +0530

    Show the user how many questions they got it right

--- 100DaysOfPython/Day17/QuizGameProject/main.py
+++ 100DaysOfPython/Day17/QuizGameProject/main.py
@@ -8,8 +8,8 @@ for q in question_data:
     question_bank.append(Question(text=q["text"], answer=q["answer"]))
 
 quiz1 = QuizBrain(question_bank)
-quiz1.next_question()
 
 while quiz1.still_has_question():
     quiz1.next_question()
 
+print("You have completed the quiz. Your final score is {}/{}.".format(quiz1.score, quiz1.question_number))
