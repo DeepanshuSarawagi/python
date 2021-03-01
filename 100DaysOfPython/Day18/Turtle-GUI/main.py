f1d9e141699bcf52fde89872c517c856ef451442
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-01 20:52:00 +0530

    Learn how to create a dashed line using turtle gui

--- 100DaysOfPython/Day18/Turtle-GUI/main.py
+++ 100DaysOfPython/Day18/Turtle-GUI/main.py
@@ -9,5 +9,14 @@ for _ in range(4):
     timmy.forward(100)
     timmy.right(90)
 
+timmy.left(90)
+
+for i in range(10):
+    timmy.forward(15)
+    timmy.penup()
+    timmy.forward(15)
+    timmy.pendown()
+
+
 screen = Screen()
 screen.exitonclick()
