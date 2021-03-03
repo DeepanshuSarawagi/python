b8a5657593b20a263a26853e67bdd2498a0081ba
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 20:20:50 +0530

    Learn how to create a prompt using screen.textinput() method

--- 100DaysOfPython/Day19/Turtle-GUI-2/turtle_race.py
+++ 100DaysOfPython/Day19/Turtle-GUI-2/turtle_race.py
@@ -3,5 +3,5 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
-
+screen.textinput("Turtle Race", "Which color turtle will win the race?")
 screen.exitonclick()
