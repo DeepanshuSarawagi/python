e3b9f191f69c8359c4f25d15a991e46f771065c2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 19:56:17 +0530

    Solve EtchASketch challenge - define functions to draw a cirlce

--- 100DaysOfPython/Day19/Turtle-GUI-2/EtchASketch.py
+++ 100DaysOfPython/Day19/Turtle-GUI-2/EtchASketch.py
@@ -26,12 +26,16 @@ def clear_screen():
     tim.reset()
 
 
+def draw_circle():
+    tim.circle(radius=100, extent=10)
+
+
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
 screen.onkey(key="a", fun=counter_clockwise)
 screen.onkey(key="d", fun=clockwise)
 screen.onkey(key="c", fun=clear_screen)
-
+screen.onkey(key="Right", fun=draw_circle)
 
 
 screen.exitonclick()
