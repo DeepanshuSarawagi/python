faae49bd3066c28eaf36cf550e1d8b8af87b4bf2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 20:58:06 +0530

    Learn about turtle.setposition()

--- 100DaysOfPython/Day19/Turtle-GUI-2/turtle_race.py
+++ 100DaysOfPython/Day19/Turtle-GUI-2/turtle_race.py
@@ -1,8 +1,12 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
-choice = screen.textinput("Turtle Race", "Which color turtle will win the race?")
-print(choice)
+guess = screen.textinput("Turtle Race", "Which color turtle will win the race?")
+
+tim = Turtle()
+tim.penup()
+tim.setposition(x=-240, y=0)
+current_color = tim.color()
+print(current_color)
 screen.exitonclick()
