14ba109b3b7ad4c02b1f54892244191ea8e0b9a2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 19:15:03 +0530

    Learn about onkey() method of Screen in turtle GUI

---
+++ 100DaysOfPython/Day19/Turtle-GUI-2/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+screen.onkey(fun=move_forward, key="Up")  # The Screen.onkey() method accepts two arguments, 1. Function  and 2. Kwy.
+# We need to ensure that when we pass a function as an argument, it is coded without parentheses. Passing the function
+# with parentheses calls the function immediately, instead we want it listen to an event and call the function when an
+# event occurs. Like for example, in our case, when a key is presses.
+
+screen.exitonclick()
