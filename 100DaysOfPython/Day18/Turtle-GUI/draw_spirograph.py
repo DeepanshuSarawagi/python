bc77f966b13058b07b1c53a09c4738781c4e9b44
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 19:00:42 +0530

    Hide turtle and experiment more with colors

--- 100DaysOfPython/Day18/Turtle-GUI/draw_spirograph.py
+++ 100DaysOfPython/Day18/Turtle-GUI/draw_spirograph.py
@@ -7,16 +7,17 @@ import random
 
 timmy = Turtle()
 timmy.speed("fastest")
+timmy.hideturtle()
 screen = Screen()
 screen.colormode(255)
 
-for _ in range(0, 361, 5):
-    R = random.randint(0, 255)
-    G = random.randint(0, 255)
-    B = random.randint(0, 255)
+for _ in range(0, 361, 4):
+    R = random.randrange(0, 255, 50)
+    G = random.randrange(0, 255, 50)
+    B = random.randrange(0, 255, 50)
     timmy.color(R, G, B)
     timmy.circle(radius=100)
-    timmy.left(5)
+    timmy.left(4)
 
 
 screen.exitonclick()
