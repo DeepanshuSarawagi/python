769d763707f0aa304c7d2e312c5d0b8573b4370f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 21:02:21 +0530

    Create colors list

--- 100DaysOfPython/Day19/Turtle-GUI-2/turtle_race.py
+++ 100DaysOfPython/Day19/Turtle-GUI-2/turtle_race.py
@@ -3,8 +3,9 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 guess = screen.textinput("Turtle Race", "Which color turtle will win the race?")
+colors = ["red", "orange", "blue", "green", "yellow", "purple"]
 
-tim = Turtle()
+tim = Turtle(shape="turtle")
 tim.penup()
 tim.setposition(x=-240, y=0)
 current_color = tim.color()
