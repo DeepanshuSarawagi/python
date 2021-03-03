29c4ae8c10eadb2dcd3f3196e1fb90c180c58b38
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-03 19:17:10 +0530

    Listen to the on screen events by calling the screen.listen() method

--- 100DaysOfPython/Day19/Turtle-GUI-2/main.py
+++ 100DaysOfPython/Day19/Turtle-GUI-2/main.py
@@ -8,6 +8,8 @@ def move_forward():
     tim.forward(10)
 
 
+screen.listen()  # In order for our turtle to listen to the screen events, we need to call this screen method
+
 screen.onkey(fun=move_forward, key="Up")  # The Screen.onkey() method accepts two arguments, 1. Function  and 2. Kwy.
 # We need to ensure that when we pass a function as an argument, it is coded without parentheses. Passing the function
 # with parentheses calls the function immediately, instead we want it listen to an event and call the function when an
