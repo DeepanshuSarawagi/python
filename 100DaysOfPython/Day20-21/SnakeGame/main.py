9ac94bd2c8a78bb5eb053a1138ba69c4f67ed8d1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-04 22:04:51 +0530

    Implement the code to have snake move forward

--- 100DaysOfPython/Day20-21/SnakeGame/main.py
+++ 100DaysOfPython/Day20-21/SnakeGame/main.py
@@ -29,9 +29,12 @@ game_is_on = True
 while game_is_on:
     screen.update()  # Turning back the animation once all the segments of snake i.e; once all the turtles
     # are created and ready to move
-    time.sleep(0.1)
-    for turtle in all_turtles:
-        turtle.forward(20)
+    time.sleep(1)
+    for turtle_num in range(len(all_turtles) - 1, 0, -1):
+        new_x = all_turtles[turtle_num - 1].xcor()
+        new_y = all_turtles[turtle_num - 1].ycor()
+        all_turtles[turtle_num].goto(new_x, new_y)
 
+    all_turtles[0].forward(20)
 
 screen.exitonclick()
