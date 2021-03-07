dde3ba84f5560d18bef342ce12a6b5910835b625
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-07 19:30:15 +0530

    create Ball class and set its position randomly

---
+++ 100DaysOfPython/Day22/PongGameProject/ball.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super(Ball, self).__init__()
+        self.shape("circle")
+        self.color("orange")
+        self.penup()
+        self.setposition((0, 0))
+
+    def move_ball(self):
+        x_pos = random.randint(-350, 350)
+        y_pos = random.randint(-300, 300)
+        self.setposition((x_pos, y_pos))
--- 100DaysOfPython/Day22/PongGameProject/main.py
+++ 100DaysOfPython/Day22/PongGameProject/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,7 +11,7 @@ screen.tracer(0)
 
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350, 0)
-
+ball = Ball()
 
 screen.listen()
 
@@ -21,5 +23,7 @@ screen.onkey(key="s", fun=left_paddle.move_paddle_down)
 game_is_on = True
 while game_is_on:
     screen.update()
+    time.sleep(0.1)
+    ball.move_ball()
 
 screen.exitonclick()
