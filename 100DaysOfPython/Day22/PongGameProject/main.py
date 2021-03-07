cbfa24bbafe1144ea3f0369b4e4e516d7b5e1dbe
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-07 13:25:34 +0530

    Create Paddle class and define methods to move it up and down

--- 100DaysOfPython/Day22/PongGameProject/main.py
+++ 100DaysOfPython/Day22/PongGameProject/main.py
@@ -1,7 +1,11 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+
+right_paddle = Paddle()
+
 screen.exitonclick()
