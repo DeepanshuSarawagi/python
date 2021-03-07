37bd07ea381a538eedd0bba65a9d9edbb2056924
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-07 14:02:46 +0530

    Turn off the animation

--- 100DaysOfPython/Day22/PongGameProject/main.py
+++ 100DaysOfPython/Day22/PongGameProject/main.py
@@ -5,11 +5,16 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
 right_paddle = Paddle()
 screen.listen()
 
 screen.onkey(key="Up", fun=right_paddle.move_paddle_up)
 screen.onkey(key="Down", fun=right_paddle.move_paddle_down)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
