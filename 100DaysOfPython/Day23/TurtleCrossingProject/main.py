5140fc76df20cea6519811d09449568367afd0ef
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-17 19:56:45 +0530

    Move the turtle up and define the function to do that

--- 100DaysOfPython/Day23/TurtleCrossingProject/main.py
+++ 100DaysOfPython/Day23/TurtleCrossingProject/main.py
@@ -8,6 +8,8 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+screen.listen()
+screen.onkey(key="Up", fun=player.go_up)
 
 game_is_on = True
 while game_is_on:
