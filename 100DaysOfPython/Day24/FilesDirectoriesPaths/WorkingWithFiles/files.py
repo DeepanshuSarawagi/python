cd77626f8238d7cfc0be206ecc0836a603defedd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-18 21:54:04 +0530

    Learn how to append contents in a file

--- 100DaysOfPython/Day24/FilesDirectoriesPaths/WorkingWithFiles/files.py
+++ 100DaysOfPython/Day24/FilesDirectoriesPaths/WorkingWithFiles/files.py
@@ -4,3 +4,14 @@ with open("my_file.txt", "r") as file:
 
 with open("my_file.txt", "w") as file:
     file.write("This is a new text")
+
+with open("new_file.txt", "w") as file:
+    file.write("Hello, this is a new file. \nI`m writing a new line here."
+               "\nThis is a third line in the text file.")
+
+with open("new_file.txt", "a") as file:
+    file.write("\nI have appended a line")
+
+with open("new_file.txt", "r") as file:
+    content = file.read()
+    print(content)
