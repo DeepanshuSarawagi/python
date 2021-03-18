d47df30eecc704f9c672368fd715a59886863fce
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-18 22:08:00 +0530

    write and read highscore from a text file

--- 100DaysOfPython/Day24/FilesDirectoriesPaths/scoreboard.py
+++ 100DaysOfPython/Day24/FilesDirectoriesPaths/scoreboard.py
@@ -6,7 +6,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super(Scoreboard, self).__init__()
         self.score = 0
-        self.high_score = 0
+        with open("highscore.txt", "r") as file:
+            data = file.read()
+        self.high_score = int(data)
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -25,10 +27,11 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("highscore.txt", "w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
     def increase_score(self):
         self.score += 1
         self.update_score()
-
