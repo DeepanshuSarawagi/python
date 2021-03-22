073f3cab7c0f23442cd3bb562bbf2627888e2cbb
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-22 20:42:01 +0530

    How to work with dictionary of lists using Pandas

--- 100DaysOfPython/Day25/CSVandPandas/pandas_basics.py
+++ 100DaysOfPython/Day25/CSVandPandas/pandas_basics.py
@@ -10,3 +10,13 @@ data = pandas.read_csv("weather_data.csv")
 print(type(data))
 print(data["temp"])
 print(type(data["temp"]))
+
+# Dataframes methods basics
+
+d = {
+    "one": [1.0, 2.0, 3.0, 4.0],
+    "two": [4.0, 2.0, 1.0, 3.0]
+     }
+
+data = pandas.DataFrame(d)
+print(data)
