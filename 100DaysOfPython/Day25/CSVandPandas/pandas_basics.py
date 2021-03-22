9e69329cd8c10ec816a7be83d1606a061d2ca9ad
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-22 20:36:14 +0530

    Underatand the data sets of pandas - Dataframes and Series

--- 100DaysOfPython/Day25/CSVandPandas/pandas_basics.py
+++ 100DaysOfPython/Day25/CSVandPandas/pandas_basics.py
@@ -5,3 +5,8 @@ with open("weather_data.csv", "r") as data_file:
     print(data)
     print()
     print(data["temp"])
+
+data = pandas.read_csv("weather_data.csv")
+print(type(data))
+print(data["temp"])
+print(type(data["temp"]))
