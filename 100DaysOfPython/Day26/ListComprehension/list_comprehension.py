6ff2a5815a88eadb9e7ad634af002be2d47f934b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-25 22:30:09 +0530

    Learn how to use list comprehension to create lists using list

---
+++ 100DaysOfPython/Day26/ListComprehension/list_comprehension.py
@@ -0,0 +1,6 @@
+# In this lesson we are going to learn about list comprehension
+# we will be creating lists using list
+
+numbers = [1, 2, 3, 4, 5]
+new_list = [(n + 1) for n in numbers]  # Here we are creating new list using existing list using list comprehension
+print(new_list)
