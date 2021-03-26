e42634bc8d45c818d1c5e3e7c7e923171f98ddf1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-26 21:20:11 +0530

    Solve the NATO alphabet project challenge - My concise solution

--- 100DaysOfPython/Day26/NATOAlphabetProject/main.py
+++ 100DaysOfPython/Day26/NATOAlphabetProject/main.py
@@ -7,11 +7,13 @@ nato_dict = {row.letter: row.code for index, row in nato_data.iterrows()}
 user_name = input("Enter your name: ")
 list_letters = [letter.upper() for letter in user_name]
 
-mnemonics = []
-for letter in list_letters:
-    # mnemonics.append([value for key, value in nato_dict.items() if key == letter]
-    for key, value in nato_dict.items():
-        if key == letter:
-            mnemonics.append(value)
+# mnemonics = []
+# for letter in list_letters:
+#     # mnemonics = ([value for key, value in nato_dict.items() if key == letter]
+#     for key, value in nato_dict.items():
+#         if key == letter:
+#             mnemonics.append(value)
 
-print(mnemonics)
+phonetics = [nato_dict[letter] for letter in list_letters if letter in nato_dict.keys()]
+
+print(phonetics)
