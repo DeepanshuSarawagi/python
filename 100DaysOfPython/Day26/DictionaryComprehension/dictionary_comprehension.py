c658682964ad5ec1ac8a6111c48ace848256d2ea
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-26 19:58:29 +0530

    learn about dictionary comprehension

--- 100DaysOfPython/Day26/DictionaryComprehension/dictionary_comprehension.py
+++ 100DaysOfPython/Day26/DictionaryComprehension/dictionary_comprehension.py
@@ -1,4 +1,17 @@
 """In this lesson we are going to learn about dictionary comprehension
 Syntax for dictionary comprehension is new_dict = {new_key:new_value for item in list}
-"""
 
+we can also create new dictionary from an existing dictionary using list comprehension
+
+new_dict = {new_key:new_value for (key, value) in dict.items()}
+
+syntax for conditional dictionary comprehension
+new_dict = {new_key:new_value for (key, value) in dict.items() if test} where test is the test condition
+
+"""
+import random
+
+names = ["Deepanshu", "Divya", "Rajat", "Vandana", "Pooja", "Krishna", "Shubham", "Lakita"]
+
+student_scores = {student: random.randint(85, 98) for student in names}
+print(student_scores)
