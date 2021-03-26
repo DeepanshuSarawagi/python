5f183c1f5a59df6b741f21d96dc0010a20ac5d8a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-26 21:50:55 +0530

    Learn how to create tkinter label

--- 100DaysOfPython/Day27/TkinterBasics/main.py
+++ 100DaysOfPython/Day27/TkinterBasics/main.py
@@ -4,4 +4,8 @@ window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
+# We will now create a label inside the tkinter window
+my_label = tkinter.Label(text="My first label")
+my_label.pack()  # We need to call this method so that the label becomes visible in the tkinter window
+
 window.mainloop()
