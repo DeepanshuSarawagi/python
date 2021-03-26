081f620cd70b78f3c45d0d4f82e20dba6d2025cf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-26 21:56:50 +0530

    Change the font of the label

--- 100DaysOfPython/Day27/TkinterBasics/main.py
+++ 100DaysOfPython/Day27/TkinterBasics/main.py
@@ -5,7 +5,7 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # We will now create a label inside the tkinter window
-my_label = tkinter.Label(text="My first label")
+my_label = tkinter.Label(text="My first label", font=("Arial", 24))
 my_label.pack()  # We need to call this method so that the label becomes visible in the tkinter window
 
 window.mainloop()
