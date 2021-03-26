369095e275a059ee682aaf42a010ecf3a4608d95
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-26 19:18:40 +0530

    Solve Day - 26 - Challenge 2

--- 100DaysOfPython/Day26/ListComprehension/list_comprehension.py
+++ 100DaysOfPython/Day26/ListComprehension/list_comprehension.py
@@ -29,3 +29,8 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 squared_numbers = [n ** 2 for n in numbers]
 print(squared_numbers)
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(even_numbers)
