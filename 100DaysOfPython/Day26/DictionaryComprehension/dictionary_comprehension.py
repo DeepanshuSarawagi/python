062e1b76e2f99797604c70106ff7dadcb9cc3fe2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-26 20:02:56 +0530

    Create dictionary from a dictionary using conditional dictionary comprehension

--- 100DaysOfPython/Day26/DictionaryComprehension/dictionary_comprehension.py
+++ 100DaysOfPython/Day26/DictionaryComprehension/dictionary_comprehension.py
@@ -13,5 +13,8 @@ import random
 
 names = ["Deepanshu", "Divya", "Rajat", "Vandana", "Pooja", "Krishna", "Shubham", "Lakita"]
 
-student_scores = {student: random.randint(85, 98) for student in names}
+student_scores = {student: random.randint(1, 100) for student in names}
 print(student_scores)
+
+passed_students = {student: score for student, score in student_scores.items() if score > 60}
+print(passed_students)
