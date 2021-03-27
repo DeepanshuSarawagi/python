858df5714acb4a29fe2e8e88d8dad4d7a22de854
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-27 21:21:21 +0530

    Create button using Tkinter

--- 100DaysOfPython/Day27/TkinterBasics/main.py
+++ 100DaysOfPython/Day27/TkinterBasics/main.py
@@ -1,19 +1,22 @@
-import tkinter
-from tkinter import messagebox
+from tkinter import *
 
-window = tkinter.Tk()
+
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # We will now create a label inside the tkinter window
-my_label = tkinter.Label(text="My first label", font=("Arial", 24))
+my_label = Label(text="My first label", font=("Arial", 24))
 my_label.pack()  # We need to call this method so that the label becomes visible in the tkinter window
-choices = [num for num in range(0, 100)]
-choicesvar = tkinter.StringVar(value=choices)
-l = tkinter.Listbox(window, listvariable=choicesvar)
-l.pack()
 
-messagebox1 = tkinter.messagebox.showinfo(title="Example", message="information")
-messagebox1.pack()
+# choices = [num for num in range(0, 100)]
+# choicesvar = tkinter.StringVar(value=choices)
+# l = tkinter.Listbox(window, listvariable=choicesvar)
+# l.pack()
+#
+# messagebox1 = messagebox.showinfo(title="Example", message="information")
+
+button = Button(text="Click Me")
+button.pack()
 
 window.mainloop()
