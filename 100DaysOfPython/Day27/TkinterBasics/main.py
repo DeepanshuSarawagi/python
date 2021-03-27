36c0a3c90257b50e3e628a17228f42c1093ef8b6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-27 13:21:54 +0530

    messagebox

--- 100DaysOfPython/Day27/TkinterBasics/main.py
+++ 100DaysOfPython/Day27/TkinterBasics/main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 
 window = tkinter.Tk()
 window.title("My First GUI Program")
@@ -7,5 +8,12 @@ window.minsize(width=500, height=300)
 # We will now create a label inside the tkinter window
 my_label = tkinter.Label(text="My first label", font=("Arial", 24))
 my_label.pack()  # We need to call this method so that the label becomes visible in the tkinter window
+choices = [num for num in range(0, 100)]
+choicesvar = tkinter.StringVar(value=choices)
+l = tkinter.Listbox(window, listvariable=choicesvar)
+l.pack()
+
+messagebox1 = tkinter.messagebox.showinfo(title="Example", message="information")
+messagebox1.pack()
 
 window.mainloop()
