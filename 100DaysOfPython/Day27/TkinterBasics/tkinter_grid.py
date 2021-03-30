c9186c586cca3e558a2a2b75d9d32ed0629e3cfe
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-30 19:49:01 +0530

    Change the layout from pack() to grid()

--- 100DaysOfPython/Day27/TkinterBasics/tkinter_grid.py
+++ 100DaysOfPython/Day27/TkinterBasics/tkinter_grid.py
@@ -13,14 +13,17 @@ window.minsize(width=500, height=500)
 
 my_label = Label(text="I Am a label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
-my_label.pack()
+# my_label.pack()
+my_label.grid(column=1, row=0)
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack()
+button.grid(column=1, row=1)
 
 input = Entry(width=10)
 print(input.get())
-input.pack()
+# input.pack()
+input.grid(column=1, row=2)
 
 
 window.mainloop()
