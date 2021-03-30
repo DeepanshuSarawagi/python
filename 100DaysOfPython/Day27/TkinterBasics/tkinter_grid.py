459c9eacc405fe2457756bbecce957f528a9f029
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-30 19:58:40 +0530

    Learn more basics about grid() geometry manager

--- 100DaysOfPython/Day27/TkinterBasics/tkinter_grid.py
+++ 100DaysOfPython/Day27/TkinterBasics/tkinter_grid.py
@@ -14,16 +14,19 @@ window.minsize(width=500, height=500)
 my_label = Label(text="I Am a label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
 # my_label.pack()
-my_label.grid(column=1, row=0)
+my_label.grid(column=0, row=0)
 
 button = Button(text="Click Me", command=button_clicked)
 # button.pack()
 button.grid(column=1, row=1)
 
+button2 = Button(text="Click Me Please!", command=button_clicked)
+button2.grid(column=2, row=0)
+
 input = Entry(width=10)
 print(input.get())
 # input.pack()
-input.grid(column=1, row=2)
+input.grid(column=3, row=2)
 
 
 window.mainloop()
