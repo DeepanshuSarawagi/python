61b2828f1cdd4715375c8f828308e4ae4921b7c0
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-30 19:41:40 +0530

    Create a button and associate the command with Entry

--- 100DaysOfPython/Day27/TkinterBasics/tkinter_grid.py
+++ 100DaysOfPython/Day27/TkinterBasics/tkinter_grid.py
@@ -1,5 +1,12 @@
 from tkinter import *
 
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
 window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=500)
@@ -7,5 +14,13 @@ window.minsize(width=500, height=500)
 my_label = Label(text="I Am a label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
 my_label.pack()
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+input = Entry(width=10)
+print(input.get())
+input.pack()
+
 
 window.mainloop()
