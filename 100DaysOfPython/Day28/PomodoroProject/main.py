e4e77b08a2cd5d46b9fee8113b42746f2f587503
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-31 20:01:10 +0530

    Add background color in both canvas and main windows

--- 100DaysOfPython/Day28/PomodoroProject/main.py
+++ 100DaysOfPython/Day28/PomodoroProject/main.py
@@ -20,8 +20,8 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro Project")
-window.config(padx=100, pady=50)
-canvas = Canvas(width=200, height=224)
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=image)
 canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
