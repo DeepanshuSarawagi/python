4f40dcba532bcd5b81b7fc88100d10d711a36b63
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-31 21:12:29 +0530

    Learn how to edit a canvas item

--- 100DaysOfPython/Day28/PomodoroProject/main.py
+++ 100DaysOfPython/Day28/PomodoroProject/main.py
@@ -18,7 +18,7 @@ LONG_BREAK_MIN = 20
 
 
 def count_down(count):
-    print(count)
+    canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, count_down, count - 1)  # We are using the Tk.after() method to do something after a certain
         # time. And to repeat it every 1 second, we are the function recursively.
@@ -29,14 +29,13 @@ def count_down(count):
 window = Tk()
 window.title("Pomodoro Project")
 window.config(padx=100, pady=50, bg=YELLOW)
-count_down(5)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
 canvas.grid(row=1, column=1, pady=20)
-
+count_down(5)
 timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(row=0, column=1)
 
