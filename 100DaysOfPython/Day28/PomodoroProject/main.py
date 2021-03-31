d6b08e1b3af13afac9e6e8239dd70f1ab871f077
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-31 21:14:25 +0530

    bind the timer function with start button

--- 100DaysOfPython/Day28/PomodoroProject/main.py
+++ 100DaysOfPython/Day28/PomodoroProject/main.py
@@ -14,6 +14,11 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
+def start_timer():
+    count_down(5)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
@@ -35,12 +40,12 @@ image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
 canvas.grid(row=1, column=1, pady=20)
-count_down(5)
+
 timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(row=0, column=1)
 
 
-start_button = Button(text="Start", bg=YELLOW, highlightthickness=0)
+start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
