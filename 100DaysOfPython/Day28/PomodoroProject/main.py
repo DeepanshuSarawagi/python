06b833cbebaa21f25db877151aa4c0aa992913a2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-03-31 21:24:27 +0530

    format the text in timer to display it as 00:00

--- 100DaysOfPython/Day28/PomodoroProject/main.py
+++ 100DaysOfPython/Day28/PomodoroProject/main.py
@@ -16,14 +16,16 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5)
+    count_down(5 * 60)  # converting 5 minutes into seconds
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = count // 60
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)  # We are using the Tk.after() method to do something after a certain
         # time. And to repeat it every 1 second, we are the function recursively.
