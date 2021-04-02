e5b7cc0ec8645dc89e4873ae1fb6747000940c5c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-02 11:35:29 +0530

    Create main window and canvas. Add image in the canvas.

--- 100DaysOfPython/Day29/PasswordGUIManagerProject/main.py
+++ 100DaysOfPython/Day29/PasswordGUIManagerProject/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(height=300, width=300, padx=20, pady=20)
+canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(row=1, column=1)
+
+window.mainloop()
