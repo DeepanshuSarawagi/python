e5d3e2a8240e890fe9f15bb69c4219a7b9307e23
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-04 12:27:52 +0530

    Create main window, canvas and add image to the canvas

--- 100DaysOfPython/Day31/FlashcardCapstoneProject/main.py
+++ 100DaysOfPython/Day31/FlashcardCapstoneProject/main.py
@@ -1,2 +1,14 @@
 BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+
+window = Tk()
+window.title("Flash Card Project")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+canvas = Canvas()
+canvas.config(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+card_front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front)
+
+window.mainloop()
 
