ebd1c20f1ada950ee526da0c9165ce09c752bd13
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-04 14:28:49 +0530

    Create a function to flip the card and show the English translation after 3 seconds

--- 100DaysOfPython/Day31/FlashcardCapstoneProject/main.py
+++ 100DaysOfPython/Day31/FlashcardCapstoneProject/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
+random_fr = {}
 
 window = Tk()
 window.title("Flash Card Project")
@@ -10,7 +11,9 @@ canvas = Canvas()
 canvas.config(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_image = canvas.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+
 right_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
 
@@ -21,16 +24,26 @@ data_dict = data.to_dict(orient="records")
 # print(data_dict)
 
 # Creating text in canvas
-title = canvas.create_text(400, 150, font=("Ariel", 34, "italic"), text="French")
-french = canvas.create_text(400, 263, font=("Ariel", 48, "bold"), text="")
+title = canvas.create_text(400, 150, font=("Ariel", 34, "italic"), text="")
+card_word = canvas.create_text(400, 263, font=("Ariel", 48, "bold"), text="")
+
+
+def flip_card():
+    canvas.itemconfig(card_image, image=card_back)
+    canvas.itemconfig(title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=random_fr["English"], fill="white")
 
 
 def create_flash_cards():
     # global french
     # canvas.delete(french)
+    global random_fr
     random_fr = random.choice(data_dict)
     # french = canvas.create_text(400, 263, font=("Ariel", 48, "bold"), text=random_fr["French"])
-    canvas.itemconfig(french, text=random_fr["French"])
+    canvas.itemconfig(card_image, image=card_front)
+    canvas.itemconfig(title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=random_fr["French"], fill="black")
+    window.after(3000, func=flip_card)
 
 
 right_button = Button(image=right_image, highlightthickness=0, command=create_flash_cards)
@@ -40,5 +53,6 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, command=create_fl
 wrong_button.grid(row=1, column=0, pady=50)
 
 create_flash_cards()
+window.after(3000, func=flip_card)
 
 window.mainloop()
