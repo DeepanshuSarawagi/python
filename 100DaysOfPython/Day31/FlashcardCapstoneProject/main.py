18bd8b050e1de2a1c1ab9e913ee7d91c5f18aff5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-04 12:32:39 +0530

    Create right and wrong button

--- 100DaysOfPython/Day31/FlashcardCapstoneProject/main.py
+++ 100DaysOfPython/Day31/FlashcardCapstoneProject/main.py
@@ -9,6 +9,14 @@ canvas.config(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
+right_image = PhotoImage(file="images/right.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1, pady=50)
+
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0, pady=50)
 
 window.mainloop()
 
