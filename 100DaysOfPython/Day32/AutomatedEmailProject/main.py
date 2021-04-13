41feb59843ca3673f1d3cb10ee8d5def31646160
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-13 15:43:54 +0530

    Try sending email using Python by generating app password in smtp provider

--- 100DaysOfPython/Day32/AutomatedEmailProject/main.py
+++ 100DaysOfPython/Day32/AutomatedEmailProject/main.py
@@ -1,8 +1,10 @@
 import smtplib
 
-my_email = "sarawagideepanshu@gmail.com"
-with smtplib.SMTP("smtp.gmail.com") as connection:
+my_email = "deepanshu.highplacesintl@yahoo.com"
+password = "gibberishpassword:P"
+
+with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
     connection.starttls()
-    connection.login(user=my_email, password="Yo!!ItIsHidden:P")
-    connection.sendmail(from_addr=my_email, to_addrs="deepanshu.highplacesintl@yahoo.com",
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="sarawagideepanshu@gmail.com",
                         msg="Subject:Hello\n\nHey There!! This is a test email using Python script")
