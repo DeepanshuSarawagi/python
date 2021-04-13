cf90bf8a5ab7fcc26d931ff5bc28575193d58be5
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-13 15:25:54 +0530

    Learn how to send email using smtplib in python

--- 100DaysOfPython/Day32/AutomatedEmailProject/main.py
+++ 100DaysOfPython/Day32/AutomatedEmailProject/main.py
@@ -1,3 +1,9 @@
 import smtplib
 
-email = smtplib.SMTP.login()
+my_email = "sarawagideepanshu@gmail.com"
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password="hidden:P")
+connection.sendmail(from_addr=my_email, to_addrs="deepanshu.highplacesintl@yahoo.com",
+                    msg="Hey There!! This is an email from a Python script")
+connection.close()
