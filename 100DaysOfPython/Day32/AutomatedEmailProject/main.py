518c2b51e41bdd77b859fff7216dd9dcc9383ea3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-13 15:37:33 +0530

    Open smtp connection using with statement

--- 100DaysOfPython/Day32/AutomatedEmailProject/main.py
+++ 100DaysOfPython/Day32/AutomatedEmailProject/main.py
@@ -1,9 +1,8 @@
 import smtplib
 
 my_email = "sarawagideepanshu@gmail.com"
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password="hidden:P")
-connection.sendmail(from_addr=my_email, to_addrs="deepanshu.highplacesintl@yahoo.com",
-                    msg="Hey There!! This is an email from a Python script")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password="Yo!!ItIsHidden:P")
+    connection.sendmail(from_addr=my_email, to_addrs="deepanshu.highplacesintl@yahoo.com",
+                        msg="Subject:Hello\n\nHey There!! This is a test email using Python script")
