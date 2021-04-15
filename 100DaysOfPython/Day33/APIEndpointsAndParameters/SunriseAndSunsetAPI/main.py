b832a8bc5a7daa3425ede7f59ffab1033b15fd67
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-15 22:00:46 +0530

    turn off the formatting as per API documentation

--- 100DaysOfPython/Day33/APIEndpointsAndParameters/SunriseAndSunsetAPI/main.py
+++ 100DaysOfPython/Day33/APIEndpointsAndParameters/SunriseAndSunsetAPI/main.py
@@ -2,9 +2,14 @@ import requests
 
 parameters = {
     "lat": 13.082680,
-    "lng": 80.270721
+    "lng": 80.270721,
+    "formatted": 0
 }
+
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+print(f"{sunrise}, {sunset}")
