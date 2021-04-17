e6f8ec0ae4de96173e7df20e8812e4ccb58edea2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-17 19:00:40 +0530

    Create canvas and label in QuizInterface class

--- 100DaysOfPython/Day34/GUIQuizAppProject/ui.py
+++ 100DaysOfPython/Day34/GUIQuizAppProject/ui.py
@@ -7,5 +7,10 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quiz App")
-
+        self.window.configure(bg=THEME_COLOR)
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white", padx=20, pady=20)
+        self.score_label.grid(row=0, column=1)
+        self.canvas = Canvas()
+        self.canvas.configure(width=300, height=250)
+        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
         self.window.mainloop()
