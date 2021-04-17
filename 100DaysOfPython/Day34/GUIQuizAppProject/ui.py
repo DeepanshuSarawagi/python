306c0f54ca8a9ec4a75b6d9d3975c754591cbbdc
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-17 19:14:51 +0530

    Add the right and wrong button in UI

--- 100DaysOfPython/Day34/GUIQuizAppProject/ui.py
+++ 100DaysOfPython/Day34/GUIQuizAppProject/ui.py
@@ -2,15 +2,27 @@ from tkinter import *
 
 THEME_COLOR = "#375362"
 
+
 class QuizInterface:
 
     def __init__(self):
         self.window = Tk()
         self.window.title("Quiz App")
-        self.window.configure(bg=THEME_COLOR)
-        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white", padx=20, pady=20)
+        self.window.configure(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1)
+
         self.canvas = Canvas()
         self.canvas.configure(width=300, height=250)
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+
+        self.true_image = PhotoImage(file="images/true.png")
+        self.false_image = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0)
+
+        self.true_button.grid(row=2, column=0)
+        self.false_button = Button(image=self.false_image, highlightthickness=0)
+
+        self.false_button.grid(row=2, column=1)
         self.window.mainloop()
