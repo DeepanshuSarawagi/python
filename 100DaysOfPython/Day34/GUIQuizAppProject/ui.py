4c98e7403eed92aa2a9b0020cb77a275bf2165aa
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-04-17 19:46:48 +0530

    Add the functionality to display the question in canvas when button is clicked

--- 100DaysOfPython/Day34/GUIQuizAppProject/ui.py
+++ 100DaysOfPython/Day34/GUIQuizAppProject/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
-
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz: QuizBrain):  # Add the datatype of quiz parameter as Class QuizBrain
+        self.quiz = quiz
+
         self.window = Tk()
         self.window.title("Quiz App")
         self.window.configure(bg=THEME_COLOR, padx=20, pady=20)
@@ -15,16 +17,21 @@ class QuizInterface:
 
         self.canvas = Canvas()
         self.canvas.configure(width=300, height=250)
-        self.question_text = self.canvas.create_text(150, 125, text="Some input text",
-                                                     fill=THEME_COLOR, font=("Arial", 28, "bold"))
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Some input text",
+                                                     fill=THEME_COLOR, font=("Arial", 20, "bold"))
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
 
         self.true_image = PhotoImage(file="images/true.png")
         self.false_image = PhotoImage(file="images/false.png")
-        self.true_button = Button(image=self.true_image, highlightthickness=0)
 
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.get_next_question)
         self.true_button.grid(row=2, column=0)
-        self.false_button = Button(image=self.false_image, highlightthickness=0)
-
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.get_next_question)
         self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text, justify="center")
