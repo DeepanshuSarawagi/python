c86825d84c31562558f6b3db19a5ee8cdb7c6ed1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-01 21:01:14 +0530

    Solve the first exercise

--- DSA/Recursion/main.py
+++ DSA/Recursion/main.py
@@ -60,3 +60,26 @@ def factorial(n):
 
 
 print(factorial(6))
+
+
+def fibonacci(n):
+    if n in [0, 1]:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+fibonacci(10)
+
+
+# Challenge 1 how to find the sum of digits of a positive integer
+
+
+def sum_of_digits(n):
+    if n <= 10:
+        return 1
+    else:
+        return int(n % 10) + sum_of_digits(n/10)
+
+
+print(sum_of_digits(12241214))
