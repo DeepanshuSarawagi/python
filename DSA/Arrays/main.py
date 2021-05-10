40341b06ce27c9eca3e463b5135751aaa3152fc3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-10 21:24:19 +0530

    learn how to create array of integer data types

---
+++ DSA/Arrays/main.py
@@ -0,0 +1,17 @@
+import array
+
+my_array = array.array('i')  # array is the module and array.array() is a class. Here we have initialized a constructor
+# of class array with data object type as int
+
+for i in range(1, 11):
+    my_array.insert(i, i * 2)
+
+list_from_array = my_array.tolist()
+
+print(my_array)
+print(list_from_array)
+
+list1 = [1, 2, 3, 4, 5]
+array2 = array.array('i')
+array2.fromlist(list1)
+print(array2)
