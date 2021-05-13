3bd4a17fa8d6db76baa4e5a6753b267626a92b2a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-13 22:17:32 +0530

    Use numpy to create 2D array

--- DSA/Arrays/TwoDimensionalArrays/main.py
+++ DSA/Arrays/TwoDimensionalArrays/main.py
@@ -8,3 +8,8 @@ for i in range(1, 11):
     two_d_array.append([i * j for j in range(2, 6)])
 
 print(two_d_array)
+
+import numpy
+
+twoDArray = numpy.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]])
+print(twoDArray)
