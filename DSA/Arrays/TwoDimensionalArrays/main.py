fff3870cb8154e426c1f3313a53bfa202299a2ac
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-15 20:31:33 +0530

    Use append operation to insert new values at the end of array

--- DSA/Arrays/TwoDimensionalArrays/main.py
+++ DSA/Arrays/TwoDimensionalArrays/main.py
@@ -15,5 +15,12 @@ print(twoDArray)
 # Insertion in 2D array
 new2DArray = numpy.insert(twoDArray, 1, [[21, 22, 23, 24]], axis=1)  # The first int parameter is the position
 # where we want to add. And axis=1 denotes we want to add new values as columns, if axis=0, add it as rows
+# Important Note: While using numpy library to insert elements in a 2-D array is that we meed to match the
+# row/column size while inserting new elements in array
 
 print(new2DArray)
+
+# We will now use the append function to insert a new row/column at the end of the array
+new2D_Array = numpy.append(twoDArray, [[97], [98], [99], [100]], axis=1)
+
+print(new2D_Array)
