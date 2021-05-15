7ce16b88d88c22f206bdcdb233494d4c5526d12d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-15 22:21:49 +0530

    basic operations on list

--- DSA/Lists/main.py
+++ DSA/Lists/main.py
@@ -1,11 +1,18 @@
 integers = [1, 2, 3, 4, 5]
 print(integers)
 
-str = ["Milk", "Eggs", "Bacon", "Cheese", "Bread"]
-print(str)
+shopping_list = ["Milk", "Eggs", "Bacon", "Cheese", "Bread"]
+print(shopping_list)
 
 mixed_list = [45, "food", 87.5, 100, "spam"]
 print(mixed_list)
 
 nested_list = [1, 2, 3, 5, [1.6, 1.7], ["spam", "eggs"]]
 print(nested_list)
+
+print("milk".title() in shopping_list)
+
+for i in shopping_list:
+    print(i)
+
+print(shopping_list[-1])
