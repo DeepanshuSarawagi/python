a26602752db5150459b1020a1d415df54083cacf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-15 20:00:12 +0530

    Insert new elements in array using numpy library

--- DSA/Arrays/TwoDimensionalArrays/main.py
+++ DSA/Arrays/TwoDimensionalArrays/main.py
@@ -11,3 +11,9 @@ print(two_d_array)
 
 twoDArray = numpy.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]])
 print(twoDArray)
+
+# Insertion in 2D array
+new2DArray = numpy.insert(twoDArray, 1, [[21, 22, 23, 24]], axis=1)  # The first int parameter is the position
+# where we want to add. And axis=1 denotes we want to add new values as columns, if axis=0, add it as rows
+
+print(new2DArray)
