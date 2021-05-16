48f4d3393df4be72393f8306f749b4cce11447fd
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-16 14:25:11 +0530

    Solve challenges

--- DSA/Lists/main.py
+++ DSA/Lists/main.py
@@ -116,3 +116,43 @@ print(" ".join(list_of_words))
 unsorted = [2, 7, 5, 1, 0, 9, 8, 11, 6]
 sorted_list = sorted(unsorted)
 print(sorted_list)
+
+a=[1,2,3,4,5]
+print(a[3:0:-1])
+
+data = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+
+
+def fun(m):
+    v = m[0][0]
+    print(v)
+
+    for row in m:
+        for element in row:
+            print("element is {} ".format(element))
+            if v < element: v = element
+
+    return v
+
+
+print(fun(data[0]))
+
+fruit_list1 = ['Apple', 'Berry', 'Cherry', 'Papaya']
+fruit_list2 = fruit_list1
+fruit_list3 = fruit_list1[:]
+
+fruit_list2[0] = 'Guava'
+fruit_list3[1] = 'Kiwi'
+
+print(fruit_list1)
+print(fruit_list2)
+print(fruit_list3)
+
+sum = 0
+for ls in (fruit_list1, fruit_list2, fruit_list3):
+    if ls[0] == 'Guava':
+        sum += 1
+    if ls[1] == 'Kiwi':
+        sum += 20
+
+print(sum)
