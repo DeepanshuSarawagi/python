74413deee0ae7a9065acab0b1b8ae3d61c7f1f3d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-16 13:53:03 +0530

    Difference b/w arrays and lists

---
+++ DSA/ArraysVsLists/main.py
@@ -0,0 +1,11 @@
+import numpy
+
+myArray = numpy.array([1, 2, 3, 4, 5, 6])
+print(myArray)
+myList = [1, 2, 3, 4, 5, 6]
+print(myList)
+
+# Advantage of array over lists is that we can perform mathematical operations on array of values but on list of values
+
+print(myArray / 2)  # This will divide every element by 2
+# print(myList / 2)  # This will error out
