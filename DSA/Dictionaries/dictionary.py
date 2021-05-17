e0a61bd626146e5dc05bfdf83ca756b3283145e3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-17 21:04:40 +0530

    Updating elements of a dictionary

--- DSA/Dictionaries/dictionary.py
+++ DSA/Dictionaries/dictionary.py
@@ -6,3 +6,12 @@ engToEsp = {
 
 print(engToEsp)
 print(engToEsp["one"])
+
+# Insert/update element in a dictionary
+my_dict = {
+    "name": "Deepanshu",
+    "age": 26
+}
+
+my_dict["age"] = 36
+print(my_dict)
