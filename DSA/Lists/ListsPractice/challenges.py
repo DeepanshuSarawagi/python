e355c74edb3a854c44e6e83d9459c40cc07ea72f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-17 19:42:39 +0530

    Return the sum of diagonal elements for a NxN matrix

--- DSA/Lists/ListsPractice/challenges.py
+++ DSA/Lists/ListsPractice/challenges.py
@@ -6,3 +6,19 @@ def middle(list1):
 
 
 print(middle(my_list))
+
+
+# Print the sum of all diagonal element:
+
+myList2D = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+def sumDiagonal(a):
+    sum_of_diagonal = 0
+    for i in range(0, len(a)):
+        print(i)
+        sum_of_diagonal += a[i][i]
+    return sum_of_diagonal
+
+
+print(sumDiagonal(myList2D))
