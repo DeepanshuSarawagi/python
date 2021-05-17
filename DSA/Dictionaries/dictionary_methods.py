784340ee87c6d64e5c7cfa3b4071faa5fd80f253
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-17 21:31:28 +0530

    create dictionary using fromkeys() method

--- DSA/Dictionaries/dictionary_methods.py
+++ DSA/Dictionaries/dictionary_methods.py
@@ -13,3 +13,10 @@ my_dict = {
 new_dict = my_dict.copy()
 print(my_dict)
 print(new_dict)
+
+"""
+fromkeys() method returns the dictionary with the given sequence of elements as keys in the form of parameters
+"""
+
+dict_2 = {}.fromkeys([1, 2, 3], 0)  # Remember that all the keys will have the same value as 0
+print(dict_2)
