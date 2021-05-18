e6b9e3c97e88bd202c1ad02b361e417e4a5d8aaf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-18 21:31:35 +0530

    Accessing elements of a tuple

--- DSA/Tuples/tuples.py
+++ DSA/Tuples/tuples.py
@@ -1,7 +1,7 @@
 new_t = ("a", "b", "c", "d", "e")
 print(new_t)
 
-# remember that tuples are unmutable and hashable.
+# remember that tuples are immutable and hashable.
 
 # If we want to create a tuple with just one element then we need to end it with a comma
 new_t2 = ("a",)
@@ -9,3 +9,6 @@ print(new_t2)
 
 tuple_1 = tuple("abcdefghijklmnopqrstuvwxyz")
 print(tuple_1)
+
+for i in range(len(tuple_1)):
+    print(tuple_1[i])
