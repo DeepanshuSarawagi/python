df7107200f83652449ebd90b001dbe3e12cb6f46
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-18 20:41:02 +0530

    Use of in operator in dictionaries

---
+++ DSA/Dictionaries/dictionary_operations.py
@@ -0,0 +1,10 @@
+my_dict = {
+    "one": "uno",
+    "two": "dos",
+    "three": "tres",
+    "four": "cuarto"
+}
+
+# Use of in operator in dictionary
+print("one" in my_dict)  # This will return True if "one" key exists in my_dict keys
+
