8d75e10e39a4849799f534fec196ee898a579c79
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-18 21:23:58 +0530

    Creating tuples

--- DSA/Tuples/tuples.py
+++ DSA/Tuples/tuples.py
@@ -2,3 +2,7 @@ new_t = ("a", "b", "c", "d", "e")
 print(new_t)
 
 # remember that tuples are unmutable and hashable.
+
+# If we want to create a tuple with just one element then we need to end it with a comma
+new_t2 = ("a",)
+print(new_t2)
