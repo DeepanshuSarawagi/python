746da79b207b4928cfcd9b10d07f5d496314deb7
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-19 21:52:08 +0530

    learn how to create singly linked list

---
+++ DSA/LinkedLists/linked_lists.py
@@ -0,0 +1,17 @@
+"""
+We will be creating singly linked lists with one head, one node and one tail.
+"""
+
+
+class Node:
+
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
