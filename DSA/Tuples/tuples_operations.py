91a61b3f0574e1745f6e6acc48126bb88e461d2e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-19 20:04:49 +0530

    Use of * operator in a tuple

--- DSA/Tuples/tuples_operations.py
+++ DSA/Tuples/tuples_operations.py
@@ -2,3 +2,5 @@ my_tuple = (1, 2, 3, 4, 5)
 my_tuple1 = (1, 2, 6, 9, 8, 7)
 
 print(my_tuple1 + my_tuple)  # Concatenating two tuples using + operator
+
+print(my_tuple * 4)  # This tuple creates a new tuple 4 times.
