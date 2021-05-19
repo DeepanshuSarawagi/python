3339335226fd702ca08767a699702942c0f35a4a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-19 20:06:48 +0530

    use of .count() method in tuple

--- DSA/Tuples/tuples_operations.py
+++ DSA/Tuples/tuples_operations.py
@@ -1,8 +1,10 @@
 my_tuple = (1, 2, 3, 4, 5)
-my_tuple1 = (1, 2, 6, 9, 8, 7)
+my_tuple1 = (1, 2, 6, 9, 8, 7, 1, 1, 1)
 
 print(my_tuple1 + my_tuple)  # Concatenating two tuples using + operator
 
 print(my_tuple * 4)  # This tuple creates a new tuple 4 times.
 
 print( 1 in my_tuple)  # in operator returns boolean if the element exists in tuple
+
+print(my_tuple1.count(1))  # This will return the count of element exists in tuple
