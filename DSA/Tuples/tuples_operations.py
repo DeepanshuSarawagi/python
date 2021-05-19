b84b6a2addd0a3d48648a468f6cd4efb5175380c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-19 20:05:52 +0530

    use of in operator in tuple

--- DSA/Tuples/tuples_operations.py
+++ DSA/Tuples/tuples_operations.py
@@ -4,3 +4,5 @@ my_tuple1 = (1, 2, 6, 9, 8, 7)
 print(my_tuple1 + my_tuple)  # Concatenating two tuples using + operator
 
 print(my_tuple * 4)  # This tuple creates a new tuple 4 times.
+
+print( 1 in my_tuple)  # in operator returns boolean if the element exists in tuple
