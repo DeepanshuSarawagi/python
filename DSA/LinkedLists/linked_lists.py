4013edeff978f82473c27097e67d13fb0ca32862
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-20 20:49:02 +0530

    Learn how to insert a new node at beginning of singly linked list

--- DSA/LinkedLists/linked_lists.py
+++ DSA/LinkedLists/linked_lists.py
@@ -1,5 +1,6 @@
 """
 We will be creating singly linked lists with one head, one node and one tail.
+SLL - is abbreviated as Singly Linked List
 """
 
 
@@ -21,4 +22,17 @@ class SinglyLinkedList:
         while node:
             yield node
             node = node.next
+
+    # Insertion in singly linked list
+    def insert_singly_linked_list(self, value, location):
+        new_node = Node(value=value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node  # By doing this we are adding the first node in our SLL
+        else:
+            if location == 0:  # If location is zero, we are inserting the element at beginning of SLL
+                new_node.next = self.head  # We are doing this because head stores node1's physical location.
+                # Hence we are setting the new node's next reference to the first node's physical location
+                self.head = new_node  # Here we are updating the head with new node's physical location
+
 
