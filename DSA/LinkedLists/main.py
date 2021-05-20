6a16db53c16b2ca04dc63ca5e5ae29ce08a6f2f1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-20 21:07:01 +0530

    test the code for inserting an element in singly linked list

--- DSA/LinkedLists/main.py
+++ DSA/LinkedLists/main.py
@@ -1,10 +1,16 @@
 import linked_lists
 
 singlyLinkedList = linked_lists.SinglyLinkedList()
-node1 = linked_lists.Node(1)
-node2 = linked_lists.Node(2)
-
-singlyLinkedList.head = node1
-singlyLinkedList.head.next = node2
-singlyLinkedList.tail = node2
+singlyLinkedList.insert_singly_linked_list(1, 1)
+singlyLinkedList.insert_singly_linked_list(2, 1)
+singlyLinkedList.insert_singly_linked_list(3, 1)
+singlyLinkedList.insert_singly_linked_list(4, 1)
+singlyLinkedList.insert_singly_linked_list(0, 0)
+singlyLinkedList.insert_singly_linked_list(6, 3)
+# node1 = linked_lists.Node(1)
+# node2 = linked_lists.Node(2)
+#
+# singlyLinkedList.head = node1
+# singlyLinkedList.head.next = node2
+# singlyLinkedList.tail = node2
 print([node.value for node in singlyLinkedList])
