3f4e8fc3df6c5dc1632e351d14597e7fbff2172b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-21 20:18:46 +0530

    Write the logic to insert the new node at the beginning of circular singly linked list

--- DSA/LinkedLists/CircularSinglyLinkedLists/circularSLL.py
+++ DSA/LinkedLists/CircularSinglyLinkedLists/circularSLL.py
@@ -18,9 +18,9 @@ class CircularSinglyLinkedList:
         node = self.head
         while node:
             yield node
-            if node.next == self.head:
-                break
             node = node.next
+            if node == self.tail.next:
+                break
 
     def create_circular_sll(self, node_value):
         node = Node(value=node_value)
@@ -28,3 +28,18 @@ class CircularSinglyLinkedList:
         self.head = node
         self.tail = node
         return "The circular singly linked list is created"
+
+    # Insertion in circular singly linked list
+    def insert_circular_sll(self, value, location):
+        if self.head is None:
+            print("The ciruclar linked list doesnt exist")
+        else:
+            new_node = Node(value=value)
+            if location == 0:  # Inserting element at beginning of circular SLL
+                new_node.next = self.head  # Here we are creating link between new node and first node. We know that
+                # head has reference to first node. So new node's next reference will be set to first node
+                self.head = new_node  # Then we will link the new and head. So head will have the reference to new node
+                # and will break the link with existing first node. Thereby, link will be like this
+                # head -> new node -> first node
+                self.tail.next = new_node  # We know that tail has the reference to last node. hence last node's next
+                # reference will be new node
