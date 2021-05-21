5037861024b9c818be43dce1bc4aae58f160b17d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-21 19:27:17 +0530

    Write the function to create circular singly linked list with just one node

---
+++ DSA/LinkedLists/CircularSinglyLinkedLists/circularSLL.py
@@ -0,0 +1,30 @@
+"""
+SLL is abbreviated as Singly Linked Lists
+"""
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
+
+    def create_circular_sll(self, node_value):
+        node = Node(value=node_value)
+        node.next = node
+        self.head = node
+        self.tail = node
+        return "The cirular singly linked list is created"
