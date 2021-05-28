fc2d28de7ace8c93eb8764ae35c2a7092797cad3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-28 20:01:41 +0530

    Learn how to create dynamic views and use placeholder in urlpatterns

--- Django/monthly_challenges/challenges/urls.py
+++ Django/monthly_challenges/challenges/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),  # Created a URL pattern. Whenever we hit the /challenges/january URI, Django will
+    # path("january", views.index),  # Created a URL pattern. Whenever we hit the /challenges/january URI, Django will
     # call the index function from views
-    path("february", views.feb_index), # Created a URL pattern. Whenever we hit the /challenges/february URI,
+    # path("february", views.feb_index), # Created a URL pattern. Whenever we hit the /challenges/february URI,
     # Django will call the index function from views
-    path("march", views.march)
+    # path("march", views.march)
+    # Instead of created urlpatterns for every URI, we can create a dynamic placeholder based on the URI accessed by the
+    # user. Django will then pass this placeholder as an argument to the views. Views will take this argument and send
+    # back the response to the user based on the function/login created
+    path("<month>", views.monthly_challenges)  # Here month is the placeholder and the syntax to create is <>
 ]
--- Django/monthly_challenges/challenges/views.py
+++ Django/monthly_challenges/challenges/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 # Remember that every view is a standalone function/class
 
 
-def index(request):
-    return HttpResponse("This Works!")  # Created a function which will return the response to a request
+# def index(request):
+#     return HttpResponse("This Works!")  # Created a function which will return the response to a request
+#
+#
+# def feb_index(request):
+#     return HttpResponse("This is a response for February index")  # Created a fn/view which will return the response
+# # for a request if URI is /challenges/february/
+#
+#
+# def march(request):
+#     return HttpResponse("learn a new skill/programming language for 20 minutes every day")
 
+# Commenting the above code briefly so that we can create dynamic views
 
-def feb_index(request):
-    return HttpResponse("This is a response for February index")  # Created a fn/view which will return the response
-# for a request if URI is /challenges/february/
-
-
-def march(request):
-    return HttpResponse("learn a new skill/programming language for 20 minutes every day")
-
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Walk for 30 minutes a day"
+    elif month == "february":
+        challenge_text = "Learn a new programming skill"
+    elif month == "march":
+        challenge_text = "Meditate for at least 20 minutes a day"
+    else:
+        return HttpResponseNotFound("Oops! Page not found.")
+    return HttpResponse(challenge_text)
