778d461788a5d1324e995aaa322f1c12e6fec719
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-29 17:27:09 +0530

    Create first model

--- Django/TiTacToe/gameplay/models.py
+++ Django/TiTacToe/gameplay/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()  # This will help us determine if first player made the move
