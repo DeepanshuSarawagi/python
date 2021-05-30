9e11e0099a0886a47a911a7462ca2b724be8d5ef
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-30 21:45:02 +0530

    Create new LinkedList class

--- DSA/LinkedLists/Practice/linkedlist.py
+++ DSA/LinkedLists/Practice/linkedlist.py
@@ -6,3 +6,15 @@ class Node:
 
     def __str__(self):
         return str(self.value)  # When we call the print function on Node object, this fn will be called
+
+
+class LinkedList:
+    def __init__(self, values=None):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
