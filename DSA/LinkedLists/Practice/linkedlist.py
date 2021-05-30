578f0656f58aea4a540ac280afbbfa7673997257
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-05-30 21:42:42 +0530

    Create new python module to practice python linkedlist

---
+++ DSA/LinkedLists/Practice/linkedlist.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return str(self.value)  # When we call the print function on Node object, this fn will be called
