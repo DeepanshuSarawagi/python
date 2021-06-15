d8497b4f471b208bc0c467de3ec512d798dbf6cf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-15 21:11:49 +0530

    render the post-details.html in views.py

--- Django/my_site/blog/views.py
+++ Django/my_site/blog/views.py
@@ -9,5 +9,5 @@ def posts(request):
     return render(request, "blog/posts.html")
 
 
-def post_detail(request):
-    return None
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
