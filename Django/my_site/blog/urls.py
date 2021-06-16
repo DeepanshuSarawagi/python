656bdfde07fc3ed1695e8570495e6423a48efa40
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-16 19:49:09 +0530

    Add some more dummy posts data in the list

--- Django/my_site/blog/urls.py
+++ Django/my_site/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
+    path("posts", views.all_posts, name="posts-page"),
     path("posts/<slug>", views.post_detail, name="post-detail-page")  # This concept of having URLs like this is
     # called as slug
 ]
--- Django/my_site/blog/views.py
+++ Django/my_site/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import date
+
 posts = [
     {"slug": "hike-in-the-mountains",
      "image": "mountains.jpg",
@@ -27,14 +28,58 @@ posts = [
             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
             mollit anim id est laborum.
             """
-     }
+     },
+
+    {"slug": "programming-is-fun",
+     "image": "coding.jpg",
+     "author": "Deepanshu",
+     "date": date(2022, 3, 10),
+     "title": "Programming Is Great!",
+     "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened "
+                "to me yesterday...",
+     "content": """
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+      aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+      velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+      aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+      velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+      aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+      velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    """
+     },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Deepanshu",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+      aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+      velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+      aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+      velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+      aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+      velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    """
+    }
 ]
+
 
 def starting_page(request):
     return render(request, "blog/index.html")
 
 
-def posts(request):
+def all_posts(request):
     return render(request, "blog/posts.html")
 
 
