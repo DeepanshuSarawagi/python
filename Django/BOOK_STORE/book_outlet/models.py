6afae84b693056dcda0c2c84fbdf65a2c72ea4f6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-16 21:58:25 +0530

    Create models for a Book class

--- Django/BOOK_STORE/book_outlet/models.py
+++ Django/BOOK_STORE/book_outlet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100)  # CharField() is a Django Model field type. This shows what kind of
+    # data we will store in title field
+    rating = models.IntegerField()  # IntegerField() is a Django Model field type. This shows what kind of
+    # data we will store in title field
