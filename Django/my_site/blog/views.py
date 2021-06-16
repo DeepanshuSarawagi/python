1eb017d8dd18729ffabacb33a21e70b74aceadf1
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-16 19:45:13 +0530

    create a list of dummy data in views.py

--- Django/my_site/blog/views.py
+++ Django/my_site/blog/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
-
+from datetime import date
+posts = [
+    {"slug": "hike-in-the-mountains",
+     "image": "mountains.jpg",
+     "author": "Deepanshu",
+     "date": date(2021, 7, 21),
+     "title": "Mountain Hiking",
+     "excerpt": """ There is nothing like the views you get when hiking in the mountains.
+                    And I wasn't even prepared for what happened whilst I was enjoying the views! """,
+     "content": """ 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum. 
+            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+            
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+            """
+     }
+]
 
 def starting_page(request):
     return render(request, "blog/index.html")
