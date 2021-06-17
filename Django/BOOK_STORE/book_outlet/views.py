0884a8e0a43b395c1081ffbbe4177fe827e733ef
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-17 18:55:39 +0530

    Create a book-detail.html and create views.py accordingly for individual book

--- Django/BOOK_STORE/book_outlet/views.py
+++ Django/BOOK_STORE/book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 # Create your views here.
@@ -8,4 +8,15 @@ def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", context={
         "books": books
+    })
+
+
+def book_detail(request, id):
+    # book = Book.objects.get(pk=id)  # pk is the default named keyword argument which means primary key
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book-detail.html", context={
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
     })
