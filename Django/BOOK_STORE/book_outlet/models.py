a5c8d8feedb39a834222eb109f17d09f9f73f056
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-17 10:43:10 +0530

    Add validators in IntegerField

--- Django/BOOK_STORE/book_outlet/models.py
+++ Django/BOOK_STORE/book_outlet/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
@@ -7,8 +7,10 @@ class Book(models.Model):
 
     title = models.CharField(max_length=100)  # CharField() is a Django Model field type. This shows what kind of
     # data we will store in title field
-    rating = models.IntegerField()  # IntegerField() is a Django Model field type. This shows what kind of
-    # data we will store in title field
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # IntegerField() is a Django Model field type. This shows what kind of
+    # data we will store in title field. We have added the validators as a Keyword arg by importing
+    # the validators class and instantiating the Min and MaxValueValidator
+    author = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.title} {self.rating}"
