8bb7af10d61cfc09e1515656341d11a9c21a1737
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-17 19:23:39 +0530

    override the get_absolute_path to construct the url instead of using the URL DTL tag in index.html

--- Django/BOOK_STORE/book_outlet/models.py
+++ Django/BOOK_STORE/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 # Create your models here.
 
 
@@ -12,6 +13,10 @@ class Book(models.Model):
     # the validators class and instantiating the Min and MaxValueValidator
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):  # We are overriding this method which automatically gets called by Django to load
+        # the specific url/page
+        return reverse("book-detail", args=[self.id])
 
     def __str__(self):
         return f"{self.title} {self.rating}"
