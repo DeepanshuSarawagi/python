38fef2b874118f38d03040498220614a92637d90
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-17 19:44:47 +0530

    Create a new field in models.py to slugify the title of the book

--- Django/BOOK_STORE/book_outlet/models.py
+++ Django/BOOK_STORE/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -13,10 +14,18 @@ class Book(models.Model):
     # the validators class and instantiating the Min and MaxValueValidator
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)  # Harry Potter 1 => harry-potter-1
 
     def get_absolute_url(self):  # We are overriding this method which automatically gets called by Django to load
         # the specific url/page
         return reverse("book-detail", args=[self.id])
+
+    # Here we are overwriting the save() method and slugifying the title of the book
+    # This slugified field will be then used in the URLs
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.slug = slugify(self.title)
+        super(Book, self).save()
 
     def __str__(self):
         return f"{self.title} {self.rating}"
