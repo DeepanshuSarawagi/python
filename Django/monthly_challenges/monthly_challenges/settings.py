b4310ab668474ee68dcc651e5b5dd5adb1542f7c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-18 21:45:23 +0530

    Update the allowed hosts lists

--- Django/monthly_challenges/monthly_challenges/settings.py
+++ Django/monthly_challenges/monthly_challenges/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8-#+=c_oagw9wf^(yf_22du#!m%4!)76_u+!edw+c!bz#-*1rm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
