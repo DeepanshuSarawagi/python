6ecce4d93cce1fe665de6ba3299cbb44b611da94
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-19 14:26:04 +0530

    Learn how to make Models Fields readonly in admin site

--- Django/BOOK_STORE/book_outlet/admin.py
+++ Django/BOOK_STORE/book_outlet/admin.py
@@ -3,4 +3,9 @@ from .models import Book
 
 # Register your models here.
 
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+
+
 admin.site.register(Book)  # This is how you register the models in admin site
