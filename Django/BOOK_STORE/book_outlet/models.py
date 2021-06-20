082252785c8c433075cafd7c870c11c2629e1a66
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-20 17:13:46 +0530

    Learn about relationships and how to build a one-to-many relationships

--- Django/BOOK_STORE/book_outlet/models.py
+++ Django/BOOK_STORE/book_outlet/models.py
@@ -5,6 +5,12 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
 
     title = models.CharField(max_length=100)  # CharField() is a Django Model field type. This shows what kind of
@@ -12,7 +18,14 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # IntegerField() is a Django Model field type. This shows what kind of
     # data we will store in title field. We have added the validators as a Keyword arg by importing
     # the validators class and instantiating the Min and MaxValueValidator
-    author = models.CharField(null=True, max_length=100)
+    # author = models.CharField(null=True, max_length=100)
+    # Above code is commented because we are creating a separate class for Author to build a one-to-many relationship
+    # between and author and their books. Hence we will set the author field to a Foreign Key field and it will accept
+    # one argument which is a model i.e., to which model we can build the relationship with
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # We have now set the relationship between book and
+    # the author. We now need to let the model know that what happens if the object with which we are building
+    # relationship gets deleted. In that case, we need to use the on_delete=CASCADE which means, delete all the related
+    # model objects before deleting the remote model object
     is_bestselling = models.BooleanField(default=False)
     # slug = models.SlugField(default="", blank=True, editable=False, null=False, db_index=True)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
