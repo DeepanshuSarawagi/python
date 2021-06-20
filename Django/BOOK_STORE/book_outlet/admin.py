faf9cd4fd4047d5d79568669e3c8e8433e493bbf
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-20 16:52:25 +0530

    Learn how to display fields as column names in django admin site

--- Django/BOOK_STORE/book_outlet/admin.py
+++ Django/BOOK_STORE/book_outlet/admin.py
@@ -8,6 +8,7 @@ class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating")
+    list_display = ("title", "rating")
 
 
 admin.site.register(Book, BookAdmin)  # This is how you register the models in admin site
