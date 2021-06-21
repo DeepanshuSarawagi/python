b079f0fe9fe90bd0394ffea074239231ad568853
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-21 09:08:04 +0530

    register Author model in the django admin site

--- Django/BOOK_STORE/book_outlet/admin.py
+++ Django/BOOK_STORE/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 
@@ -11,4 +11,9 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "rating")
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+
 admin.site.register(Book, BookAdmin)  # This is how you register the models in admin site
+admin.site.register(Author, AuthorAdmin)
