e45fbcd83fffb06da059d2f3066de9ed9c412767
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-23 19:25:42 +0530

    Handle form submission and extract data

--- Django/feedback/reviews/views.py
+++ Django/feedback/reviews/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 
 def review(request):
+    if request.method == "POST":
+        entered_username = request.POST['username']
+        print(entered_username)
+        redirect_path = reverse("thankyou")
+        return HttpResponseRedirect(redirect_path)
     return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
