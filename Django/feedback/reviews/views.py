ce022147fb8cc80f1c6aa982473ef4287b7f3d6c
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-23 19:40:52 +0530

    Manually handle the form validation and send the feedback to the user

--- Django/feedback/reviews/views.py
+++ Django/feedback/reviews/views.py
@@ -8,10 +8,16 @@ from django.urls import reverse
 def review(request):
     if request.method == "POST":
         entered_username = request.POST['username']
+        if entered_username == "":
+            return render(request, "reviews/review.html", context={
+                "has_error": True
+            })
         print(entered_username)
         redirect_path = reverse("thankyou")
         return HttpResponseRedirect(redirect_path)
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", context={
+        "has_error": False
+    })
 
 
 def thank_you(request):
