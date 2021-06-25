06cebd2600cbb360177f66006cddc674c577fe8f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-25 09:56:22 +0530

    customized error messages in the form field

--- Django/feedback/reviews/forms.py
+++ Django/feedback/reviews/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=100, label="Your Name")
+    user_name = forms.CharField(max_length=100, label="Your Name", error_messages={
+        "required": "Your name cannot be empty",
+        "max_length": "Name cannot exceed 100 characters"
+    })
