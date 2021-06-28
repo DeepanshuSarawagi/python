7965983d9b78a85ddaeafde0fa8107f5e654080e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-06-28 20:23:05 +0530

    Add two new fields in form

--- Django/feedback/reviews/forms.py
+++ Django/feedback/reviews/forms.py
@@ -6,3 +6,5 @@ class ReviewForm(forms.Form):
         "required": "Your name cannot be empty",
         "max_length": "Name cannot exceed 100 characters"
     })
+    review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
