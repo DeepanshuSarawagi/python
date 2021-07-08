6960758e4964e83530d4ec0907b0643f2fa0bb90
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-07-08 20:34:04 +0530

    Create a form by using FormField and interpolate it to render in the create_profile.html

--- Django/feedback/profiles/views.py
+++ Django/feedback/profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
-
+from .forms import ProfileForm
 # Create your views here.
 
 
@@ -14,7 +14,10 @@ def store_file(file):
 class CreateProfileView(View):
 
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", context={
+            "form": form
+        })
 
     def post(self, request):
         store_file(request.FILES["image"])
