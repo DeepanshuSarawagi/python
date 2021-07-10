e1c0e275389c74ee4ae5ab7251c6ccbb3b9a3715
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-07-10 12:13:01 +0530

    Configure session cookie age

--- Django/feedback/feedback/settings.py
+++ Django/feedback/feedback/settings.py
@@ -130,3 +130,4 @@ MEDIA_URL = '/user-media/'  # Django does not expose the project directories to 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_COOKIE_AGE = 1800  # Configuring session to be active for 30 minutes
