cd4982c5c8f324770161d47a649d486fbb6df66b
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-08-11 21:19:53 +0530

    create new fields in the UserProfile model

--- DjangoRestFramework/profiles-rest-api/profiles_api/models.py
+++ DjangoRestFramework/profiles-rest-api/profiles_api/models.py
@@ -8,3 +8,10 @@ from django.contrib.auth.models import PermissionsMixin
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database models for the user in the system"""
     email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
