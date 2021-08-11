9fd89fab874a5424b76affb2ee74028b29b69fb0
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-08-11 21:11:29 +0530

    Create new user model

--- DjangoRestFramework/profiles-rest-api/profiles_api/models.py
+++ DjangoRestFramework/profiles-rest-api/profiles_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for the user in the system"""
+    email = models.EmailField(max_length=255, unique=True)
