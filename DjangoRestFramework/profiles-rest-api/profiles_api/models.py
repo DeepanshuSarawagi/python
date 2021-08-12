f6e46319e32643bb8be0f66958068709d93bcc54
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-08-12 19:13:29 +0530

    define a fn to create a superuser

--- DjangoRestFramework/profiles-rest-api/profiles_api/models.py
+++ DjangoRestFramework/profiles-rest-api/profiles_api/models.py
@@ -16,7 +16,15 @@ class UserProfileManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(name=name, email=email)
         user.set_password(password)
-        user.save(self._db)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create super user with given details"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
         return user
 
 
