90888c7b3d9641e86dd17c44d2c8fab6c80c8a09
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-08-23 19:59:57 +0530

    register the viewset with the router

--- DjangoRestFramework/profiles-rest-api/profiles_api/urls.py
+++ DjangoRestFramework/profiles-rest-api/profiles_api/urls.py
@@ -5,6 +5,8 @@ from profiles_api import views
 # This is how we register a viewset which we created in the views.py
 router = DefaultRouter()
 router.register("hello-viewset", views.HelloViewSet, base_name='hello-viewset')
+router.register("profile-viewset", views.UserProfileViewSet)  # If we define the queryset in the view, we need not
+# provide the base_name in the router.register
 
 urlpatterns = [
     path('hello-view', views.HelloAPIView.as_view()),
