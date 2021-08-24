4449b477517233c81a4f4c0564dd6ef3bab78124
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2021-08-24 21:12:14 +0530

    Create a view for UserLoginAPI and add the default renderer_classes

--- DjangoRestFramework/profiles-rest-api/profiles_api/views.py
+++ DjangoRestFramework/profiles-rest-api/profiles_api/views.py
@@ -8,6 +8,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloAPIView(APIView):
@@ -125,3 +127,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating User Authentication Tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
