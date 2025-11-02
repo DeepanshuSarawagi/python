1993268c41a8d18024959abcd7cf2e332eb66966
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-02 17:30:54 +0530

    New example

---
+++ Introduction/strings2.py
@@ -0,0 +1,8 @@
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+
+# Negative indexing
+print(parrot[-1])  # Will print the last character of the string
+print(parrot[-14]) # Will print the first character of the string
+print(parrot[:9:2]) # Stepper, it will skip every 1 character until index 9
