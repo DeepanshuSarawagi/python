ebbe34367ae44832758a9c53d78f60b7e31b458e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-03 13:05:22 +0530

    Learn about string formatting left alignment in replacement fields

--- Introduction/formatting.py
+++ Introduction/formatting.py
@@ -1,5 +1,14 @@
 for i in range(1,13):
     print("No. {:2} squared is {:3} and cubed is {:4}".format(i,i**2,i**3)) #Field replacement width
-
+print()
 for i in range(1,13):
     print("No. {:<2} squared is {:<3} and cubed is {:<4}".format(i,i**2,i**3)) # Field replacement left alignment
+
+print()
+print("Pi is approximately {0:12}".format(22 / 7))  # Default with 15 precision points of 15 digits float number post decimal
+print("Pi is approximately {0:.2f}".format(22 / 7)) # Exactly two precision points of 2 digits float number post decimal
+print("Pi is approximately {0:12.12f}".format(22 / 7)) # Exactly two precision points of 12 digits float number post decimal
+print("Pi is approximately {0:12.50f}".format(22 / 7)) # Exactly two precision points of 50 digits float number post decimal
+print("Pi is approximately {0:52.50f}".format(22 / 7)) # The 12, 52, 62, 72 are field width
+print("Pi is approximately {0:62.50f}".format(22 / 7))
+print("Pi is approximately {0:72.50f}".format(22 / 7))
