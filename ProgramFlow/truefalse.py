87cd9a4523d4fc110f284b1b5d6866432626d979
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-04 11:28:21 +0530

    Boolean expression

--- ProgramFlow/truefalse.py
+++ ProgramFlow/truefalse.py
@@ -1,8 +1,19 @@
 day = "Monday"
 temperature = 30
-raining = True
+raining = False
 
-if day == "Saturday" and temperature > 27 and not raining:
+if day == "Saturday" and temperature > 27 or not raining:
     print("Go swimming")
 else:
     print("Learn Python")
+
+if 0:               # O, empty sequences except string are evaluated to False
+    print("True")
+else:
+    print("False")
+
+name = input("What is your name? ")
+if name:
+    print("Hello, {}".format(name))
+else:
+    print("Are you the man with no name?")
