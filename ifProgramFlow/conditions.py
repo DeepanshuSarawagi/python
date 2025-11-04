11cbf4a635de522b40cde920e4648f6c8d06a6ba
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-04 11:11:50 +0530

    While loop

---
+++ ifProgramFlow/conditions.py
@@ -0,0 +1,18 @@
+import random
+
+age = int(input("What is your age? "))
+
+if 16 <= age <= 65:
+    print("Have a good day at work!")
+else:
+    print("Sorry, you are not old enough to do that")
+
+answer = 5
+guess = 6
+while guess != answer:
+    guess = random.randint(1, 11)
+    print("New random number is:", guess)
+    if guess == answer:
+        print("You guessed the number")
+    else:
+        print("Keep guessing the number")
