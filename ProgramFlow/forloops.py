dcce24e700018ac4c840a09e8df4ebb3e12bc7e4
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-05 11:40:04 +0530

    Use of break in for loop

--- ProgramFlow/forloops.py
+++ ProgramFlow/forloops.py
@@ -51,3 +51,15 @@ for index in range(len(shopping_list)):
         break
 
 print("Found {} at index {}".format(item_to_find, found_at))
+
+item_to_find = "albatross"
+found_at = None
+for index in range(len(shopping_list)):
+    if shopping_list[index] == item_to_find:
+        found_at = index
+        break
+
+if found_at is not None:
+    print("Found {} at index {}".format(item_to_find, found_at))
+else:
+    print("{} not found".format(item_to_find))
