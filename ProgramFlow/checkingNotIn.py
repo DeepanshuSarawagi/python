dc911713babc203eae3a778056a8f1cc007cafb7
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-05 09:23:19 +0530

    In/Not in operator

--- ProgramFlow/checkingNotIn.py
+++ ProgramFlow/checkingNotIn.py
@@ -1,3 +1,13 @@
 activity = input("What would you like to do today? ")
 if "cinema" not in activity.casefold():
     print("I want to go to cinema!")
+
+# Challenge
+
+name = str(input("What is your name? "))
+age = int(input("What is your age? "))
+
+if 18 <= age < 31:
+    print("Enjoy your holiday, {}!".format(name))
+else:
+    print("Sorry! But the holiday packages are not available.!")
