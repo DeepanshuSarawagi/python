55a1c263a3d95b02f17ccd6e1312709bec4e92f2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-11-11 20:31:53 +0530

    simple coding exercise

--- ProgramFlow/high_low_binary_chop.py
+++ ProgramFlow/high_low_binary_chop.py
@@ -21,3 +21,12 @@ while True:
     else:
         print("Please enter h, l or c.")
     guesses += 1
+
+number = 5
+multiplier = 8
+answer = 0
+
+for _ in range(multiplier):
+    answer += number
+
+print(answer)
