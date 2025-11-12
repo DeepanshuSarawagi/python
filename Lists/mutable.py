15427311e8edd36674c35f78a646bceb573a6fd5
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2025-11-12 10:25:45 +0530

    Update mutable.py

--- Lists/mutable.py
+++ Lists/mutable.py
@@ -11,3 +11,10 @@ Since list is mutable, the id's are same for both lists unlike boolean and strin
 print(shopping_list)
 print(id(shopping_list))
 print(id(another_list))
+
+"""One thing to note here is that there is just one list in this python program. This is because both `shopping_list and `another_list` are pointing to the same object. 
+Any change made to the object will be reflected in both lists.
+"""
+
+print(another_list is shopping_list)
+print(another_list)
