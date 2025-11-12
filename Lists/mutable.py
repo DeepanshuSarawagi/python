270500dba22b42c4e1cfdabc8d82d78b0e8f1968
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2025-11-12 10:27:49 +0530

    Update mutable.py

--- Lists/mutable.py
+++ Lists/mutable.py
@@ -18,3 +18,20 @@ Any change made to the object will be reflected in both lists.
 
 print(another_list is shopping_list)
 print(another_list)
+
+"""To confirm the theory, we can use the following code:
+Plus we can see that the id's are same which means they are pointing to the same object.
+"""
+a = b = c = d = e = f = another_list
+print(a is another_list)
+print(b is another_list)
+print(c is another_list)
+print(d is another_list)
+print(e is another_list)
+print(f is another_list)
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+print(id(e))
+print(id(f))
