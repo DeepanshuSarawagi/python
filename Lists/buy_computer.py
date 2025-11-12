2b77b0ae3532b798bc723fb5ea09a4256b3c186e
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2025-11-12 12:47:36 +0530

    Update buy_computer.py

--- Lists/buy_computer.py
+++ Lists/buy_computer.py
@@ -1,22 +1,13 @@
 available_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi cable"]
 current_choice = "-"
 computer_parts = []
-
+valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+print(valid_choices)
 while current_choice != "0":
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print("Adding {}".format(current_choice))
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice == "2":
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice == "4":
-            computer_parts.append("mouse")
-        elif current_choice == "5":
-            computer_parts.append("mouse mat")
-        elif current_choice == "6":
-            computer_parts.append("hdmi cable")
+        computer_parts.append(available_parts[int(current_choice) - 1])
+
     else:
         print("Please add options from the list below:")
         """enumerate function adds a counter to an iterable and returns it"""
