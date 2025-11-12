6745c5d138205acbcd70661c59f571a50a111137
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2025-11-12 10:38:20 +0530

    Create number_lists.py

---
+++ Lists/number_lists.py
@@ -0,0 +1,19 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+numbers = even + odd
+print(sorted(numbers))
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+print(len(numbers))
+
+print()
+
+print(len(even))
+print(len(odd))
+
+print()
+
+print("mississippi".count("s"))
+print("mississippi".count("iss"))
