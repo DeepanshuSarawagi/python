2b20deb13f0a30658cfab60a752a44cc4fffb214
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-12-01 16:39:21 +0530

    Learn more about join method

---
+++ Lists/joining_things.py
@@ -0,0 +1,12 @@
+flowers = [
+    "Daffodil",
+    "Sunflower",
+    "Tiger Lily",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "Lavender"
+]
+
+output = " | ".join(flowers)
+print(output)
