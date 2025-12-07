1df97b2cbaa2aa35d6f0f867dae15a25db78e43e
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-12-07 12:00:30 +0530

    Tuples continued

---
+++ Tuples/tuples_continued.py
@@ -0,0 +1,8 @@
+welcome = "Welcome to my Nightmare!", "Alice Cooper", 1975
+bad = "Bad Company", "bad Company", 1974
+budgie = "NightFlight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the lighting", "Metallica", 1984
+
+metallica[0] = "Master of puppets"  # This will not work since tuples are immmutable. We can't change the item in the tuples
+print(metallica)
