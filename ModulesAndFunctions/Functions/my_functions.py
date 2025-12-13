ecc717eac98f0bb8525af1900b112bc49fa128a3
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-12-13 17:47:24 +0530

    functions intro

--- ModulesAndFunctions/Functions/my_functions.py
+++ ModulesAndFunctions/Functions/my_functions.py
@@ -1,6 +1,6 @@
-def multiply():
-    result = 10.5 * 4
+def multiply(x, y):
+    result = x * y
     return result
 
 if __name__ == "__main__":
-    print(multiply())
+    print(multiply(12469, 9012470))
