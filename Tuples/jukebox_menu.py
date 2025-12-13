a565be3bc8c203699c4261efe5231db4b703b669
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-12-13 17:19:13 +0530

    User input for song selection

--- Tuples/jukebox_menu.py
+++ Tuples/jukebox_menu.py
@@ -1,14 +1,24 @@
 from nested_data import albums
 
+SONGS_LIST = 3
+SONG_TITLE_INDEX = 1
+
 while True:
     print("Please choose the album to play (invalid choice exists): ")
     for index, (title, artist, year, songs)  in enumerate(albums):
         print("{}: {}".format(index + 1, title))
     choice = int(input("Enter a choice: "))
     if 1 <= choice <= len(albums):
-        songs_list = albums[choice - 1][3]
+        songs_list = albums[choice - 1][SONGS_LIST]
     else:
         break
-    print(albums[choice - 1])
-    print(songs_list)
-    print()
+    print("Please choose your song: ")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
+    song_choice = int(input("Enter a choice: "))
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+    else:
+        break
+    print("Playing: ", title)
+    print("=" * 40)
