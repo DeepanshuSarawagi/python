8211d2368e4267921cee4d824d9f8708330750a8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2025-12-13 13:32:52 +0530

    User input in the jukebox_menu.py project

--- Tuples/jukebox_menu.py
+++ Tuples/jukebox_menu.py
@@ -3,5 +3,12 @@ from nested_data import albums
 while True:
     print("Please choose the album to play (invalid choice exists): ")
     for index, (title, artist, year, songs)  in enumerate(albums):
-        print("{} - {}, {}, {}, {}".format(index + 1, title, artist, year, songs))
-    break
+        print("{}: {}".format(index + 1, title))
+    choice = int(input("Enter a choice: "))
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice - 1][3]
+    else:
+        break
+    print(albums[choice - 1])
+    print(songs_list)
+    print()
